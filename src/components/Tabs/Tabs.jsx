1a78fe5957105c7a7da8b231bf143a568b42ad39
Liza Strykharchuk <ol220681vol@gmail.com>
2023-06-27 14:30:55 +0300

    code

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,23 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => (
+  <ul>
+    {tabs.map(tab => (
+      <li
+        key={tab.id}
+        className={cn({
+          'is-active': selectedTabId === tab.id,
+        })}
+        data-cy="Tab"
+      >
+        <a
+          href={`#${tab.id}`}
+          data-cy="TabLink"
+          onClick={() => onTabSelected(tab.id)}
+        >
+          {tab.title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
