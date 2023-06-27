5bc45160fd84b5a31051aceaeb34d78ebfc1e4c6
Yura Enttsel <equikmla@gmail.com>
2023-06-27 13:50:34 +0300

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,22 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = (Props) => {
+  const { tabId, tabTitle, activeId, setId } = Props;
+
+  return (
+    <li
+      className={cn({
+        'is-active': tabId === activeId,
+      })}
+      data-cy="Tab"
+    >
+      <a
+        href={`#${tabId}`}
+        data-cy="TabLink"
+        onClick={() => setId(tabId)}
+      >
+        {tabTitle}
+      </a>
+    </li>
+  );
+};
