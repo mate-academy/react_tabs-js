acbe72cc1531221a0415d2d865248f5587fd4731
Olena Shevchenko <shevochkas@gmail.com>
2023-06-28 21:44:07 +0300

    React components were added

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,41 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+  selectedTab,
+}) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {
+          tabs.map(tab => (
+            <li
+              className={`${selectedTabId === tab.id
+                ? 'is-active'
+                : ''
+              }`}
+              key={tab.id}
+              data-cy="Tab"
+            >
+              <a
+                onClick={() => {
+                  onTabSelected(tab);
+                }}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
