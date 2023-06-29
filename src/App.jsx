a3bbfaf8b5113d68a1c56e6c76e587cecac31626
Ir-ra <chapter.ku@ukr.net>
2023-06-29 16:38:40 +0300

    tabs 3

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,15 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-// eslint-disable-next-line no-shadow
-const useSelectedTab = (tabs) => {
-  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
-  const foundTab = tabs.find(tab => tab.id === selectedTabId);
+const useSelectedTab = (givenTabs) => {
+  const [selectedTabId, setSelectedTabId] = useState(givenTabs[0].id);
+  const foundTab = givenTabs.find(tab => tab.id === selectedTabId);
 
-  const handleSelectingTab = tabId => (setSelectedTabId(tabId));
+  const handleSelectingTab = (tabId) => {
+    if (selectedTabId !== tabId) {
+      setSelectedTabId(tabId);
+    }
+  };
 
   return [foundTab, handleSelectingTab];
 };
@@ -23,11 +26,12 @@ const useSelectedTab = (tabs) => {
 export const App = () => {
   const preparedTabs = [...tabs];
   const [foundTab, handleSelectingTab] = useSelectedTab(preparedTabs);
+  const { title } = foundTab;
 
   return (
     <div className="section">
       <h1 className="title">
-        {`Selected tab is ${foundTab.title}`}
+        {`Selected tab is ${title}`}
       </h1>
       <Tabs
         tabs={preparedTabs}
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,33 +1,37 @@
 import classNames from 'classnames';
 
-export const Tabs = ({ tabs, onTabSelected, foundTab }) => (
-  <div data-cy="TabsComponent">
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map((tab) => {
-          const { id, title } = tab;
+export const Tabs = ({ tabs, onTabSelected, foundTab }) => {
+  const { id: foundId, content } = foundTab;
 
-          return (
-            <li
-              className={classNames(id === foundTab.id ? 'is-active' : '')}
-              data-cy="Tab"
-              key={id}
-            >
-              <a
-                href={`#${id}`}
-                data-cy="TabLink"
-                onClick={() => onTabSelected(id)}
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                className={classNames(id === foundId ? 'is-active' : '')}
+                data-cy="Tab"
+                key={id}
               >
-                {title}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => onTabSelected(id)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
-    <div className="block" data-cy="tab-content">
-      {foundTab.content}
+      <div className="block" data-cy="tab-content">
+        {content}
+      </div>
     </div>
-  </div>
-);
+  );
+};
