6a35ad5d47a4732868689162ab157a00b85cf6d3
Oksana Tytanych <oksanatytanych@gmail.com>
2023-06-30 09:52:32 +0300

    react_tabs-js

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,20 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({
+  tab,
+  selectedTabId,
+  setSelectedTabId,
+}) => (
+  <li
+    data-cy="Tab"
+    className={cn({ 'is-active': selectedTabId === tab.id })}
+  >
+    <a
+      href={`#${tab.id}`}
+      data-cy="TabLink"
+      onClick={() => setSelectedTabId(tab.id)}
+    >
+      {tab.title}
+    </a>
+  </li>
+);
