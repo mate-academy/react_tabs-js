ddd7687867be288c9ee5a46568d75834aa4c840f
Hanna Vasylieva <ann0950368600@gmail.com>
2023-06-30 10:18:42 +0100

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const isValidTabId = tabs.some(tab => tab.id === selectedTabId);
+
+  if (!isValidTabId) {
+    // eslint-disable-next-line no-param-reassign
+    selectedTabId = tabs[0].id;
+  }
+
+  const handleTabClick = (tab) => {
+    if (selectedTabId !== tab.id) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={selectedTabId === tab.id ? 'is-active' : ''}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => handleTabClick(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {tabs.map(t => (selectedTabId === t.id ? (
+        <div className="block" data-cy="TabContent" key={t.id}>
+          {t.content}
+        </div>
+      ) : null))}
+    </div>
+  );
+};
