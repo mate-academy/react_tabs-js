c149d62819d83f5202aa82d4309577ee47d6a6fe
Aleks <offsset@gmail.com>
2023-07-01 17:04:45 +0300

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,32 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ selectedTabId, setselectedTabId, tabs }) => {
+  // eslint-disable-next-line no-console
+  console.log(tabs);
+
+  return (
+    <ul>
+      {tabs.map(({ id, title }) => {
+        const isActive = id === selectedTabId;
+
+        return (
+          <li
+            key={id}
+            className={cn({ 'is-active': isActive })}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${id}`}
+              data-cy="TabLink"
+              onClick={() => {
+                setselectedTabId(id);
+              }}
+            >
+              {title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
