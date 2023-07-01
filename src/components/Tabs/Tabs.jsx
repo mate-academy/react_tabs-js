c599f852ada2a0439e48be21275f34a46c8ecd82
Aleks <offsset@gmail.com>
2023-07-01 16:35:37 +0300

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,21 +1,27 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import cn from 'classnames';
 
 export const Tabs = ({ selectedTabId, setselectedTabId, tabs }) => (
   <ul>
-    {
-      tabs.map(tab => (
-        // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+    {tabs.map(({ id, title }) => {
+      const isActive = id === selectedTabId;
+
+      return (
         <li
-          key={tab.id}
-          className={cn({ 'is-active': tab.id === selectedTabId })}
+          key={id}
+          className={cn({ 'is-active': isActive })}
           data-cy="Tab"
-          onClick={() => {
-            setselectedTabId(tab.id);
-          }}
         >
-          <a href={`#${tab.id}`} data-cy="TabLink">{tab.title}</a>
+          <a
+            href={`#${id}`}
+            data-cy="TabLink"
+            onClick={() => {
+              setselectedTabId(id);
+            }}
+          >
+            {title}
+          </a>
         </li>
-      ))}
+      );
+    })}
   </ul>
 );
