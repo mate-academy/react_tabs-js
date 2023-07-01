cdd1827d1c0539a0235af69b6a5abf5b645d5755
Aleks <offsset@gmail.com>
2023-07-01 15:05:36 +0300

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,17 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ selectedTabId, setselectedTabId, tabs }) => (
+  <ul>
+    {
+      tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={cn({ 'is-active': tab.id === selectedTabId })}
+          data-cy="Tab"
+          onClick={ () => { setselectedTabId(tab.id)}}
+        >
+          <a href={`#${tab.id}`} data-cy="TabLink">{tab.title}</a>
+        </li>
+      ))}
+  </ul>
+);
