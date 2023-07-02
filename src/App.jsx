b03f1fd449471277a1ca99d5a5f477d368de9596
alex <cebanoleksandr@gmail.com>
2023-07-02 12:00:34 +0300

    add App one more time

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,9 @@ export const App = () => {
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is {currentTab.title}
+        Selected tab is
+        {' '}
+        {currentTab.title}
       </h1>
 
       <Tabs
