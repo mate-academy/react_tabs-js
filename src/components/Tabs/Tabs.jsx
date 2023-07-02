fa60154c50d8a898e8c287c173feb5484ca69427
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-07-02 17:56:45 +0300

    moved check to function

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -5,34 +5,40 @@ export const Tabs = ({
   selectedTabId,
   selectedTab,
   onTabSelected,
-}) => (
-  <div data-cy="TabsComponent">
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            key={tab.id}
-            className={cn({ 'is-active': selectedTabId === tab.id })}
-            data-cy="Tab"
-          >
-            <a
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-              onClick={() => {
-                if (tab.id !== selectedTabId) {
-                  onTabSelected(tab);
-                }
-              }}
-            >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+}) => {
+  const checkSelect = (tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
 
-    <div className="block" data-cy="TabContent">
-      {selectedTab.content}
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={cn({ 'is-active': selectedTabId === tab.id })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  checkSelect(tab);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
     </div>
-  </div>
-);
+  );
+};
