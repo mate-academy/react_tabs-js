81ad3f081d2c60491a5dc18893eeee50ea27a6e2
DefaultGitUser <usingforgit@gmail.com>
2023-07-03 11:40:48 +0300

    Sol

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,50 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const getTabContent = tabId => tabs.find(tab => tab.id === tabId)
+    ?.content;
+
+  const activeTabId = tabs.find(tab => tab.id === selectedTabId)?.id
+    || tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={cn({
+                'is-active': tab.id === activeTabId,
+              })}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (tab.id !== activeTabId) {
+                    onTabSelected(tab);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div
+        className="block"
+        data-cy="TabContent"
+      >
+        {getTabContent(activeTabId) || 'No content' }
+      </div>
+    </div>
+  );
+};
