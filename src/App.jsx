cfc64e6da635c0a835421679780a9234980f2db8
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-07-04 23:03:07 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import cn from 'classnames';
+import { TabContent } from './components/Tabs';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Tabs } from './components/Tabs';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -12,69 +12,40 @@ export const tabs = [
 ];
 
 export const App = () => {
-  const [selected, setSelected] = useState('tab-1');
-  const [title, setTitle] = useState('');
+  const [selectedTab, setSelected] = useState(tabs[0]);
 
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is
-        {title}
+        {`Selected tab is ${selectedTab.title}`}
       </h1>
 
       <div data-cy="TabsComponent">
         <div className="tabs is-boxed">
           <ul>
-            <li
-              className={cn({ 'is-active': selected === 'tab-1' })}
-              data-cy="Tab"
-            >
-              <a
-                href="#tab-1"
-                data-cy="TabLink"
-                onClick={() => {
-                  setSelected('tab-1');
-                }}
-              >
-                Tab 1
-              </a>
-            </li>
-
-            <li
-              data-cy="Tab"
-              className={cn({ 'is-active': selected === 'tab-2' })}
-            >
-              <a
-                href="#tab-2"
-                data-cy="TabLink"
-                onClick={() => {
-                  setSelected('tab-2');
-                }}
-              >
-                Tab 2
-              </a>
-            </li>
-
-            <li
-              data-cy="Tab"
-              className={cn({ 'is-active': selected === 'tab-3' })}
-            >
-              <a
-                href="#tab-3"
-                data-cy="TabLink"
-                onClick={() => {
-                  setSelected('tab-3');
-                }}
-              >
-                Tab 3
-              </a>
-            </li>
+            {
+              tabs.map(tab => (
+                <li
+                  className={cn({ 'is-active': selectedTab.id === tab.id })}
+                  data-cy="Tab"
+                  key={tab.id}
+                >
+                  <a
+                    href="#tab-1"
+                    data-cy="TabLink"
+                    onClick={() => {
+                      setSelected(tab);
+                    }}
+                  >
+                    {tab.title}
+                  </a>
+                </li>
+              ))
+            }
           </ul>
         </div>
-        <Tabs
-          selected={selected}
-          tabs={tabs}
-          sendTitle={setTitle}
+        <TabContent
+          selectedTab={selectedTab}
         />
       </div>
     </div>
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,11 +1,5 @@
-export const Tabs = ({ selected, tabs, sendTitle }) => {
-  const obj = tabs.find(item => item.id === selected);
-
-  sendTitle(obj.title);
-
-  return (
-    <div className="block" data-cy="TabContent">
-      {obj.content}
-    </div>
-  );
-};
+export const TabContent = ({ selectedTab }) => (
+  <div className="block" data-cy="TabContent">
+    {selectedTab.content}
+  </div>
+);
