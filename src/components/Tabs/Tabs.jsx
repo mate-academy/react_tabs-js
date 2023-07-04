89c419ef9868f6bd54e23dd6e68d2e4c4608d87f
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-07-04 19:32:00 +0300

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,11 @@
-export const Tabs = () => {};
+export const Tabs = ({ selected, tabs, sendTitle }) => {
+  const obj = tabs.find(item => item.id === selected);
+
+  sendTitle(obj.title);
+
+  return (
+    <div className="block" data-cy="TabContent">
+      {obj.content}
+    </div>
+  );
+};
