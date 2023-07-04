785d8b3dd837a1bd41fc84514e4452a2900bd9dc
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-07-04 18:57:58 +0300

    task done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,49 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+const setId = id => `#${id}`;
+
+export const Tabs = ({
+  tabs,
+  onTabSelected,
+  selectedTabId,
+  table,
+}) => {
+  const checkChange = (tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab || tabs[0]);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({
+                'is-active': tab.id === selectedTabId,
+              })}
+            >
+              <a
+                href={setId(tab.id)}
+                data-cy="TabLink"
+                onClick={() => checkChange(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div
+        className="block"
+        data-cy="TabContent"
+      >
+        {table.content}
+      </div>
+    </div>
+  );
+};
