ee7f63d9959c1939cc89f3eaac0cd44ca4ee7cb4
AndriiYelieva <andrii.yelieva@gmail.com>
2023-07-06 10:55:52 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
+
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -9,32 +11,52 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">
-      Selected tab is Tab 1
-    </h1>
+export const App = () => {
+  const [value, setValue] = useState('tab-1');
+  const [titleH1, setTitleH1] = useState('Tab 1');
+  const [text, setText] = useState('Some text 1');
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">Tab 1</a>
-          </li>
+  return (
+    <div className="section">
+      <h1 className="title">
+        {`Selected tab is ${titleH1}`}
+      </h1>
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">Tab 2</a>
-          </li>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(({ id, title, content }) => {
+              const isSelected = id === value;
 
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">Tab 3</a>
-          </li>
-        </ul>
-      </div>
+              return (
+                <li
+                  key={id}
+                  data-cy="Tab"
+                  className={classnames({
+                    'is-active': isSelected,
+                  })}
+                >
+                  <a
+                    href={`#${id}`}
+                    data-cy="TabLink"
+                    onClick={() => {
+                      setValue(id);
+                      setTitleH1(title);
+                      setText(content);
+                    }}
+                  >
+                    {title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
 
-      <div className="block" data-cy="TabContent">
-        Some text 1
+        <div className="block" data-cy="TabContent">
+          {text}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
