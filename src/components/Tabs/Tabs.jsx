bb4364d7c600e636018afbeb88c715b30458b9b7
Diana Tuz <dianatuz12@gmail.com>
2023-07-08 14:40:32 +0300

    done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  const getSelectedTab = (tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({ 'is-active': selectedTab.id === tab.id })}
+              onClick={() => getSelectedTab(tab)}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))
+  }
+        </ul>
+      </div>
+      <div
+        className="block"
+        data-cy="TabContent"
+      >
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
