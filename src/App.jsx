a0fb4c43a8295a595c43357d21886501ab7821b4
haduigon <haduigon@gmail.com>
2023-07-11 21:58:37 +0300

    next commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,23 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import { Tabs } from './components/Tabs';
 
-interface Tab {
-  id: string,
-  title: string,
-  content: string,
-}
-
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App: React.FC = () => {
+export const App = () => {
   const [selectedTab, setSelectedTab] = useState(tabs[0]);
-  const currentTab = (tab: Tab) => setSelectedTab(tab);
+  const currentTab = tab => setSelectedTab(tab);
+  const selectedTabId = selectedTab.id.slice(-1);
 
   return (
     <div className="section">
       <h1 className="title">
         Selected tab is Tab
         &nbsp;
-        {selectedTab.id.slice(-1)}
+        {selectedTabId}
       </h1>
 
       <div data-cy="TabsComponent">
