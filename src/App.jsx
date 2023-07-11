743db95770ebfb751cefc55bc1e87c221b2741be
haduigon <haduigon@gmail.com>
2023-07-11 21:26:25 +0300

    first commit, copy of react_tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs';
+
+interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -9,32 +16,31 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">
-      Selected tab is Tab 1
-    </h1>
+export const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+  const currentTab = (tab: Tab) => setSelectedTab(tab);
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">Tab 1</a>
-          </li>
+  return (
+    <div className="section">
+      <h1 className="title">
+        Selected tab is Tab
+        &nbsp;
+        {selectedTab.id.slice(-1)}
+      </h1>
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">Tab 2</a>
-          </li>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <Tabs
+            tabs={tabs}
+            currentTab={currentTab}
+            selected={selectedTab}
+          />
+        </div>
 
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">Tab 3</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="block" data-cy="TabContent">
-        Some text 1
+        <div className="block" data-cy="TabContent">
+          {selectedTab.content}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,32 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, currentTab, selected }) => {
+  function handleTab(selectedTab, tab) {
+    if (selectedTab !== tab) {
+      currentTab(tab);
+    }
+  }
+
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          className={classNames({
+            'is-active': selected === tab,
+          })}
+          key={tab.id}
+          data-cy="Tab"
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={() => handleTab(selected, tab)}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
