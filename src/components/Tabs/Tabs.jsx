a3d86d090cba70ac0100adee34117eede293a124
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-07-19 12:51:19 +0300

    made solution but 3 test still dont pass

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+const ACTIVE_STYLE = 'is-active';
+
+export const Tabs = ({
+  tabs,
+  selectedId,
+  onTabSelected,
+}) => {
+  const className = tabId => ((tabId === selectedId
+    || (!selectedId && tabId === tabs[0].id))
+    ? ACTIVE_STYLE : null);
+
+  return (
+    tabs.map((tab) => {
+      const { id, title } = tab;
+
+      const linkEventHandler = (event) => {
+        event.preventDefault();
+        if (selectedId !== id) {
+          onTabSelected(tab);
+        }
+      };
+
+      return (
+        <li
+          key={id}
+          data-cy="Tab"
+          className={className(id)}
+        >
+          <a
+            data-cy="TabLink"
+            href={`#${id}`}
+            onClick={linkEventHandler}
+          >
+            {title}
+          </a>
+        </li>
+      );
+    }));
+};
