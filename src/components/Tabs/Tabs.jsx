7f37f23a5ae0c422c4956467590f242ed1b2e0d0
Nazar Vorona <voron1765@gmail.com>
2023-07-20 00:41:16 +0300

    all test done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,42 +1,36 @@
 import cn from 'classnames';
 
 export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
-  const selectedTab = tabs.find(tab => tab.id === selectedTabId);
+  const currentTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
 
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map((tab) => {
-            const { id, title } = tab;
-
-            const handleChangeTab = () => {
-              if (selectedTabId !== id) {
-                onTabSelected(id);
-              }
-            };
-
-            return (
-              <li
-                key={id}
-                className={cn({ 'is-active': selectedTabId === id })}
-                data-cy="Tab"
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={cn({ 'is-active': currentTab.id === tab.id })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (currentTab.id !== tab.id) {
+                    onTabSelected(tab);
+                  }
+                }}
               >
-                <a
-                  href={`#${id}`}
-                  data-cy="TabLink"
-                  onClick={handleChangeTab}
-                >
-                  {title}
-                </a>
-              </li>
-            );
-          })}
+                {tab.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-        {selectedTab.content}
+        {currentTab.content}
       </div>
     </div>
   );
