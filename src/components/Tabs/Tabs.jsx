ad9511b7019db3e85b112df89967afe3011a4e62
Max <maxpusto533gmail.com>
2023-07-23 13:00:29 +0300

    pre_result

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,30 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, setSelectedTabs, state, setState, action }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            className={state === tab.id ? 'is-active' : ''}
+            key={tab.id}
+            data-cy="Tab"
+          >
+            <a
+              onClick={() => action(tab.id)}
+              href="#tab-1"
+              data-cy="TabLink"
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {tabs.map(tab => (
+        `${state === tab.id ? tab.content : ''}`
+      ))}
+    </div>
+  </div>
+);
