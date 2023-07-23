a97d19fe04c24e06c3a1f560ab46f286dc6fdb41
Max <maxpusto533gmail.com>
2023-07-23 18:43:54 +0300

    added cn, && logic

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,31 +1,33 @@
+import cn from 'classnames';
+
 export const Tabs = ({ tabs, setSelectedTabs, state, setState,
-    onTabSelected }) => (
-  <div data-cy="TabsComponent">
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            className={state === tab.id ? 'is-active' : ''}
-            key={tab.id}
-            data-cy="Tab"
-          >
-            <a
-              onClick={() => onTabSelected(tab.id)}
-              href="#tab-1"
-              data-cy="TabLink"
+  onTabSelected }) => (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({ 'is-active': state === tab.id })}
+              key={tab.id}
+              data-cy="Tab"
             >
-              {tab.title}
-            </a>
-          </li>
+              <a
+                onClick={() => onTabSelected(tab.id)}
+                href="#tab-1"
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.map(tab => (
+          `${state === tab.id ? tab.content : ''}`
         ))}
-
-      </ul>
+      </div>
     </div>
-
-    <div className="block" data-cy="TabContent">
-      {tabs.map(tab => (
-        `${state === tab.id ? tab.content : ''}`
-      ))}
-    </div>
-  </div>
 );
