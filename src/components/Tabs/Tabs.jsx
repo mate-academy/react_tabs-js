e76eb08dfe31cc33df37d0ef1f4174fdce1d6789
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-07-28 15:57:17 +0200

    I dont know why some tests dont pass

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,23 @@
-export const Tabs = () => {};
+import { Tab } from '../Tab/Tab';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <Tab
+            key={tab.id}
+            onTabSelected={onTabSelected}
+            selectedTabId={selectedTabId}
+            tab={tab}
+            tabs={tabs}
+          />
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {tabs.find(tab => tab.id === selectedTabId).content}
+    </div>
+  </div>
+);
