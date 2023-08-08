c9a15375e713e2033fc8c4477ae88b541381f147
Dima Korol <korovaidima@gmail.com>
2023-08-08 12:26:32 +0200

    first commit

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({
+  tabs,
+  selectedTabId,
+  selectedTab,
+  onTabSelected,
+}) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={cn(
+              { 'is-active': tab.id === selectedTabId },
+            )}
+            data-cy="Tab"
+          >
+            <a
+              data-cy="TabLink"
+              href={`#${tab.id}`}
+              onClick={() => {
+                if (tab.id !== selectedTabId) {
+                  onTabSelected(tab.id);
+                }
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
