34e6a568213da9129b2223399628f22d3d2a6d33
Horizon-git <apbreloaded947@gmail.com>
2023-08-08 18:53:04 +0300

    add destructing

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,36 +1,42 @@
 import cn from 'classnames';
 
 export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
-  const currentTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+  const { id: currentId, content: currentContent } = tabs.find(
+    tab => tab.id === selectedTabId,
+  ) || tabs[0];
 
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
-            <li
-              className={cn({ 'is-active': tab.id === currentTab.id })}
-              data-cy="Tab"
-              key={tab.id}
-            >
-              <a
-                href={`#${tab.id}`}
-                data-cy="TabLink"
-                onClick={() => {
-                  if (currentTab.id !== tab.id) {
-                    onTabSelected(tab);
-                  }
-                }}
+          {tabs.map((tab) => {
+            const { id: tabId, title: tabTitle } = tab;
+
+            return (
+              <li
+                className={cn({ 'is-active': tabId === currentId })}
+                data-cy="Tab"
+                key={tabId}
               >
-                {tab.title}
-              </a>
-            </li>
-          ))}
+                <a
+                  href={`#${tabId}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (currentId !== tabId) {
+                      onTabSelected(tab);
+                    }
+                  }}
+                >
+                  {tabTitle}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-        {currentTab.content}
+        {currentContent}
       </div>
     </div>
   );
