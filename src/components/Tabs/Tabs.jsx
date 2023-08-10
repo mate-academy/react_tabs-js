db7265ad63bd8087897dc24349b59f3c9a2fe177
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-08-10 10:56:07 +0300

    test fixed

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -5,7 +5,10 @@ export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => (
         {tabs.map(tab => (
           <li
             key={tab.id}
-            className={tab.id === selectedTabId ? 'is-active' : ''}
+            className={(tab.id === selectedTabId 
+            || (!tabs.some(t => t.id === selectedTabId) && tab === tabs[0]))
+            ? 'is-active' 
+            : ''}
             data-cy="Tab"
           >
             <a
@@ -25,7 +28,7 @@ export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => (
       </ul>
     </div>
 
-    <div className="block" data-cy="tab-content">
+    <div className="block" data-cy="TabContent">
       {(tabs.find(tab => tab.id === selectedTabId) || tabs[0]).content}
     </div>
   </div>
