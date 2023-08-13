8590efed15dfb79c19574417d8f7a54ceee87b33
OlegRak86 <rakolegit@gmail.com>
2023-08-13 18:11:03 +0300

    react_tabs-js

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,19 @@
-export const Tabs = () => {};
+export const Tabs = ({ tab, selectedTabId, onTabSelected, setTabItem }) => (
+  <li
+    data-cy="Tab"
+    className={`${selectedTabId === tab.id && 'is-active'}`}
+  >
+    <a
+      onClick={() => {
+        onTabSelected(tab.id);
+        setTabItem(tab);
+      }
+      }
+      href={`#${tab.id}`}
+      data-cy="TabLink"
+    >
+      {tab.title}
+
+    </a>
+  </li>
+);
