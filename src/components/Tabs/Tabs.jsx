82df43d9b54c4c3486300becb3e98cfb4f7540ee
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-08-21 12:33:46 +0300

    commit

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,34 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({ tabs, value, setValue }) => {
+  const activeTab = tabs.find(tab => tab.title === value);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(table => (
+            <li
+              className={`${value === table.title && 'is-active'}`}
+              data-cy="Tab"
+            >
+              <a
+                key={table.id}
+                onClick={() => setValue(table.title)
+                }
+                href={`#${table.id}`}
+                data-cy="TabLink"
+              >
+                {table.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab && activeTab.content}
+      </div>
+    </div>
+  );
+};
