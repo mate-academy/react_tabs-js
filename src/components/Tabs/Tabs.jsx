8132543b4e2dd6d18f46ca1d9258b998ade433bb
Dmytro Fefelov <reseneweb@gmail.com>
2023-08-23 16:43:54 +0300

    optimized the condition for the is-active class as much as possible and moved it to the isActive variable, created the handleTabClick function for use when clicking on the tab, destructured the tab

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -5,38 +5,45 @@ export const Tabs = ({
   selectedTabId,
   setSelectedTabId,
   onTabSelected,
-}) => (
-  <div
-    data-cy="TabsComponent"
-  >
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            data-cy="Tab"
-            className={cn({ 'is-active':
-            (!tabs.find(item => (item.id === selectedTabId)) && tabs[0] === tab)
-            || (selectedTabId) === tab.id })}
-            key={tab.id}
-          >
-            <a
-              data-cy="TabLink"
-              href={`#${tab.id}`}
-              onClick={() => {
-                if (tab.id !== selectedTabId) {
-                  onTabSelected(tab);
-                }
-              }}
-            >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+}) => {
+  const handleTabClick = (tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
 
-    <div className="block" data-cy="TabContent">
-      {(tabs.find(tab => tab.id === selectedTabId) || tabs[0]).content}
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
+            const isActive
+            = (!tabs.some(item => item.id === selectedTabId) && tabs[0] === tab)
+            || selectedTabId === tab.id;
+
+            return (
+              <li
+                key={id}
+                data-cy="Tab"
+                className={cn({ 'is-active': isActive })}
+              >
+                <a
+                  data-cy="TabLink"
+                  href={`#${id}`}
+                  onClick={() => handleTabClick(tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {(tabs.find(tab => tab.id === selectedTabId) || tabs[0]).content}
+      </div>
     </div>
-  </div>
-);
+  );
+};
