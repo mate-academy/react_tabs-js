11a5e2bab9237be4d0413b396a8fe47ab5faab71
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-25 23:33:45 +0300

    initial commit with mistakes

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,32 @@
-export const Tabs = () => {};
+import React from 'react';
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectTabId, setSelectTabId, activeTab }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(({ title, id }) => (
+          <li
+            key={id}
+            className={cn({ 'is-active': id === selectTabId })}
+            data-cy="Tab"
+          >
+            <a
+              onClick={() => setSelectTabId(id)}
+              href={`#${id}`}
+              data-cy="TabLink"
+            >
+              {title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div
+      className="block"
+      data-cy="TabContent"
+    >
+      {activeTab.content}
+    </div>
+  </div>
+);
