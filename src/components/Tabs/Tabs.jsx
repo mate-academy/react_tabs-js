acb99f1d7d90ab4c1d3069262814010eb61e967f
Sergey Arnautov <sirius9312@ukr.net>
2023-08-25 13:47:45 +0300

    Application

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,43 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const chooseTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {
+            tabs.map(tab => (
+              <li
+                key={tab.id}
+                className={tab.id === chooseTab.id ? 'is-active' : ''}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  onClick={() => (
+                    tab.id !== selectedTabId && (
+                      onTabSelected(tab)
+                    )
+                  )}
+                  data-cy="TabLink"
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {
+          tabs.map(tab => (
+            selectedTabId === tab.id && (
+              tab.content
+            )
+          ))
+        }
+      </div>
+    </div>
+  );
+};
