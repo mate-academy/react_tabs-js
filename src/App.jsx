baae1d762592059599b63fb9fe2cc10776cf5683
Maryna Kyrychynska <m_kirichok@ukr.net>
2023-08-29 15:26:05 +0300

    Tabs_fixed_2

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,19 @@ export const tabs = [
 ];
 
 export const App = () => {
-  const [selectedTabId, setSelectedTabId] = useState(tabs[0]);
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+  const handleTabSelected = ({ id }) => setSelectedTabId(id);
 
   return (
     <div className="section">
       <h1 className="title">
-        {`Selected tab is ${selectedTabId.title}`}
+        {`Selected tab is ${tabs.find(tab => tab.id === selectedTabId).title}`}
       </h1>
 
       <Tabs
         tabs={tabs}
-        selectedTabId={selectedTabId.id}
-        onTabSelected={setSelectedTabId}
+        selectedTabId={selectedTabId}
+        onTabSelected={handleTabSelected}
       />
     </div>
   );
