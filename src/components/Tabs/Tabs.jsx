85912cdbc02afefb7d2100edbd28b87d01aab092
Vladyslav Prokhasko <vlad56445@gmail.com>
2023-08-29 16:48:49 +0300

    Implementation of the App and Tabs components for displaying and switching between the specified tabs.

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,42 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const defaultSelectedTab = tabs.find(
+    tab => tab.id === selectedTabId,
+  ) || tabs[0];
+
+  const handleTabClick = (tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={tab.id === defaultSelectedTab.id
+                ? 'is-active'
+                : ''
+              }
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => handleTabClick(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === defaultSelectedTab.id)?.content}
+      </div>
+    </div>
+  );
+};
