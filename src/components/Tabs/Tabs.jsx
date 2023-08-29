90a649f848aaf8f580cdc66384dadf498a39bfdc
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-08-29 15:17:11 +0200

    Added version with error

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,29 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = (tabs, { selectedTabId }, onTabSelected) => (
+  <>
+    <ul>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={cn({ 'is-active': () => tab.id === selectedTabId })}
+          data-cy="Tab"
+        >
+          <a
+            onClick={() => {
+              onTabSelected(tab.id);
+            }}
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))
+      }
+    </ul>
+    <div className="block" data-cy="TabContent">
+      {tabs.filter()}
+    </div>
+  </>
+);
