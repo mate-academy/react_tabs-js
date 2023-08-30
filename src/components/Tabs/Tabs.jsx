19ab3d113dd845705be8228b189b310d1a0ddae8
KlymSudakov <sudakov.klym@gmail.com>
2023-08-31 01:29:27 +0300

    Implemented App and Tabs components to render and switch between tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+import { getTabById } from '../../utils';
+
+export const Tabs = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = getTabById(tabs, selectedTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                className={cn({
+                  'is-active': selectedTab.id === id,
+                })}
+                data-cy="Tab"
+                key={id}
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => id !== selectedTab.id && onTabSelected(tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
+export * from './utils';
