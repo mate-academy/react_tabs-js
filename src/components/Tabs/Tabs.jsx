8cee7f7294bb01a4174e3b90f98199cc1972cdc3
Anton Bilous <toxanteloxran@gmail.com>
2023-08-31 14:40:53 +0300

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,13 @@
-export const Tabs = () => {};
+export const Tabs = ({
+  tab: { id = 'tab-1', title, content }, setValue, value,
+}) => (
+  <li data-cy="Tab" className={value === id ? 'is-active' : ''}>
+    <a
+      href={`#${id}`}
+      data-cy="TabLink"
+      onClick={() => setValue(id)}
+    >
+      {title}
+    </a>
+  </li>
+);
