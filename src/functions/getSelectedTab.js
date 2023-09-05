78d4d31ed01d0141d4643f6fc028c69698c0801f
4xmplme <andreykav2002@gmail.com>
2023-09-05 11:44:18 +0300

    remove unused code

--- src/functions/getSelectedTab.js
+++ src/functions/getSelectedTab.js
@@ -1,4 +1,3 @@
 export function getSelectedTab(tabs, selectedTabId) {
-  // return tabs.find(tab => tab.id === selectedTabId) || tabs[0];
   return tabs.find(tab => tab.id === selectedTabId) || tabs[0];
 }
