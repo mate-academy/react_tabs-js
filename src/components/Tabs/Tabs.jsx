f03b3d97d41c0400ef10c18a644f82bbc8f58ada
Юрий Мисюра <apriory13@meta.ua>
2023-09-16 09:52:28 +0300

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,49 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({
+  tabs,
+  selectedTabId,
+  onSelectedTabIdChange,
+}) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  const handleTabChange = (id) => {
+    if (id !== selectedTabId) {
+      onSelectedTabIdChange(id);
+    }
+  };
+
+  return (
+    <div className="section">
+      <h1 className="title">
+        {`Selected tab is ${selectedTab.title}`}
+      </h1>
+
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                className={cn({ 'is-active': selectedTab.id === tab.id })}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleTabChange(tab.id)}
+                >
+                  {`${tab.title}`}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {`${selectedTab.content}`}
+        </div>
+      </div>
+    </div>
+  );
+};
