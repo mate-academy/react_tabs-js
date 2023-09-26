259fe1cd420f28e39df9a46861052d1a014b9b5b
Volodymyr Biletskyi <volodymyrbiletskyi92@gmail.com>
2023-09-26 20:47:33 +0300

    solution2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import classnames from 'classnames';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -10,17 +11,20 @@ export const tabs = [
 ];
 
 export const App = () => {
-  const [selectedTabId, setSelectedTabId] = useState('tab-1');
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
 
   const handleTabSelected = (tabId) => {
     setSelectedTabId(tabId);
   };
 
+  const selectedTabTitle = tabs.find(tab => tab.id === selectedTabId).title;
+  const selectedTabContent = tabs.find(tab => tab.id === selectedTabId).content;
+
   return (
     <div className="section">
       <h1 className="title">
         Selected tab is
-        {tabs.find(tab => tab.id === selectedTabId).title}
+        {selectedTabTitle}
       </h1>
 
       <div data-cy="TabsComponent">
@@ -29,7 +33,9 @@ export const App = () => {
             {tabs.map(tab => (
               <li
                 key={tab.id}
-                className={selectedTabId === tab.id ? 'is-active' : ''}
+                className={
+                  classnames({ 'is-active': selectedTabId === tab.id })
+                }
                 data-cy="Tab"
               >
                 <a
@@ -45,7 +51,7 @@ export const App = () => {
         </div>
 
         <div className="block" data-cy="TabContent">
-          {tabs.find(tab => tab.id === selectedTabId).content}
+          {selectedTabContent}
         </div>
       </div>
     </div>
