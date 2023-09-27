83ee43a99e1ad20197c16a7bb8a5ebaf6ecd5de5
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-09-27 22:55:46 +0200

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,25 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({
+  tabs,
+  selectedTabId,
+  setSelectedTabId,
+}) => (
+  <ul>
+    {tabs.map(tab => (
+      <li
+        className={cn({ 'is-active': selectedTabId === tab })}
+        data-cy="Tab"
+        key={tab.id}
+      >
+        <a
+          href={`#${tab.id}`}
+          data-cy="TabLink"
+          onClick={() => setSelectedTabId(tab)}
+        >
+          {tab.title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
