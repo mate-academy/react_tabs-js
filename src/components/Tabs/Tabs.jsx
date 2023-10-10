6a4d94f083aca06c3b91a10e43f550d7861e973d
Vovchuk <kulivackijvoloda@gmail.com>
2023-10-10 20:15:06 +0300

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+export const Tabs = (
+  { tabs, selectedTabId, onTabSelected },
+) => {
+  const TabClick = (tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tabs.id}
+              className={tab.id === selectedTabId ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => TabClick(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === selectedTabId)
+          ?.content
+        }
+      </div>
+    </div>
+  );
+};
