6f489f60b9b66749a2897ec2fc3c8235da435cc5
Denys <denzzlle171@gmail.com>
2023-10-11 19:45:05 +0300

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,25 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTabId, handleSelectedTab }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+          <li
+            key={tab.id}
+            className={selectedTabId === tab.id ? 'is-active' : ''}
+            data-cy="Tab"
+            onClick={() => selectedTabId !== tab.id && handleSelectedTab(tab)}
+          >
+            <a href={`#${tab.id}`} data-cy="TabLink">
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {`Some text ${selectedTabId.split('-')[1]}`}
+    </div>
+  </div>
+);
