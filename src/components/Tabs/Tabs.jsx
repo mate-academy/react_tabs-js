65501e9eeb8b0ce6c75adaffbe4a6cbe18481c5e
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2023-10-11 20:19:26 +0300

    Switching tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const isActiveTabPresent = tabs.some(tab => tab.id === selectedTabId);
+  const verifiedSelectedTabId = isActiveTabPresent ? selectedTabId : tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab, index) => (
+            <li
+              data-cy="Tab"
+              key={tab.id}
+              className={tab.id === verifiedSelectedTabId ? 'is-active' : ''}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (tab.id !== verifiedSelectedTabId) {
+                    onTabSelected(tab);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === selectedTabId)?.content}
+      </div>
+    </div>
+  );
+};
