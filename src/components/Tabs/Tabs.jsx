ec93150b9644be9fefd6b7f2769fcd0796835006
OlhaKozuik <gadyakaban@gmail.com>
2023-10-11 22:14:24 +0300

    first solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,32 @@
-export const Tabs = () => {};
+import 'bulma/css/bulma.css';
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, id, setChange }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(table => (
+          <li
+            className={cn({
+              'is-active': id === table.id,
+            })}
+            data-cy="Tab"
+            key={table.id}
+          >
+            <a
+              href={`#${table.id}`}
+              data-cy="TabLink"
+              onClick={() => id !== table.id && setChange(table.id)}
+            >
+              {table.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {tabs.find(tab => tab.id === id).content}
+    </div>
+  </div>
+);
