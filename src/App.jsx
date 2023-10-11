efe4e66c72c7b7867d37a247e2756d20df23ffd0
Oksana Maruniak <syper.ksenka@gmail.com>
2023-10-11 13:45:41 +0300

    incomplete code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -11,66 +11,19 @@ export const tabs = [
 ];
 
 export const App = () => {
-  const [value, setValue] = useState(1);
-  const [message, setMessage] = useState(`Some text`);
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
 
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is Tab {value}
+        Selected tab is Tab 1
       </h1>
 
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            <li
-              className={cn({ 'is-active': setValue })}
-              data-cy="Tab"
-            >
-              <a
-                onClick={() => {
-                  setValue(1);
-                  setMessage(message);
-                }}
-                href="#tab-1"
-                data-cy="TabLink"
-              >
-                Tab 1
-              </a>
-            </li>
-
-            <li className={cn({ 'is-active': setValue })} data-cy="Tab">
-              <a
-                onClick={() => {
-                  setValue(2);
-                  setMessage(message);
-                }}
-                href="#tab-2"
-                data-cy="TabLink"
-              >
-                Tab 2
-              </a>
-            </li>
-
-            <li className={cn({ 'is-active': setValue })} data-cy="Tab">
-              <a
-                onClick={() => {
-                  setValue(3);
-                  setMessage(message);
-                }}
-                href="#tab-3"
-                data-cy="TabLink"
-              >
-                Tab 3
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          {`${message} ${value}`}
-        </div>
-      </div>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTabId}
+        setSelectedTabId={setSelectedTabId}
+      />
     </div>
   );
 };
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({
+  tabs,
+  selectedTabId,
+  setSelectedTabId,
+}) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            className={cn({ 'is-active': selectedTabId === tab.id })}
+            data-cy="Tab"
+            key={tab.id}
+          >
+            <a
+              onClick={() => {
+                setSelectedTabId(tab.id);
+              }}
+              href="#tab-1"
+              data-cy="TabLink"
+            >
+              Tab 1
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      Some text 1
+    </div>
+  </div>
+);
