d3e5dd6d4715d82de0d0bfa2f3666433e992f423
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-10-16 11:15:14 +0300

    add isSelectedTab variable

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -7,23 +7,27 @@ export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
-            <li
-              className={cn({
-                'is-active': selectedTab.id === tab.id,
-              })}
-              data-cy="Tab"
-              key={tab.id}
-            >
-              <a
-                href={`#${tab.id}`}
-                data-cy="TabLink"
-                onClick={() => selectedTabId !== tab.id && onTabSelected(tab)}
+          {tabs.map((tab) => {
+            const isSelectedTab = selectedTab.id === tab.id;
+
+            return (
+              <li
+                className={cn({
+                  'is-active': isSelectedTab,
+                })}
+                data-cy="Tab"
+                key={tab.id}
               >
-                {tab.title}
-              </a>
-            </li>
-          ))}
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => !isSelectedTab && onTabSelected(tab)}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
 
