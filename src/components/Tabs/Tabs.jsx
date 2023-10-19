3c00702a1f337e624d7d0c40d5873a9fd7fff7f9
Daniil <danil.beshlebnov@gmail.com>
2023-10-19 14:45:43 +0300

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,14 @@
-export const Tabs = () => {};
+import { Tab } from '../Tab/Tab';
+
+export const Tabs = ({ tabs, selectedTab, handleTabClick }) => (
+  <ul>
+    {tabs.map(tab => (
+      <Tab
+        key={tab.id}
+        tab={tab}
+        isActive={selectedTab.id === tab.id}
+        handleTabClick={() => handleTabClick(tab)}
+      />
+    ))}
+  </ul>
+);
