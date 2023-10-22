7b1670748974c074201fc88eaad6502e40294363
Denys Shurubura <denys.shurubura@gmail.com>
2023-10-22 14:34:15 +0300

    task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,45 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, currentTab, onTabSelected }) => {
+  let selectedTab = tabs.find(tab => tab.id === currentTab.id) || tabs[0];
+
+  if (selectedTab === undefined) {
+    // eslint-disable-next-line prefer-destructuring
+    selectedTab = tabs[0];
+  }
+
+  const HandleClick = (tab) => {
+    if (tab.id !== selectedTab) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              key={tab.id}
+              className={cn({
+                'is-active': selectedTab.id === tab.id,
+              })}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => HandleClick(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
