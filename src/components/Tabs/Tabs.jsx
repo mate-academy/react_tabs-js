74156fdcc5ddc714ced44eeb71ebb6dcf252af0b
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-10-23 23:43:43 +0300

    add logics to App and Tabs components

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,38 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId = 'tab-1', onTabSelected }) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  const currentTabId = tabs.find(tab => tab.id === selectedTabId)
+    ? selectedTabId : tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({ 'is-active': currentTabId === tab.id })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                onClick={() => (currentTabId !== tab.id
+                  ? onTabSelected(tab) : '')}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+
+  );
+};
