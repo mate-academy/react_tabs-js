33961e0dd27f914ae1c2096c4c2e5dd01ec7d143
Vladislav Balaban <vlad.balaban2020@gmail.com>
2023-10-23 13:18:49 +0300

    Added tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, onTabSelect, selectTabId }) => {
+  const selectedTab = tabs.find(tab => tab.id === selectTabId) || tabs[0];
+  const handleClick = (tab) => {
+    if (selectTabId !== tab.id) {
+      onTabSelect(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              classNames={classNames({
+                'is-active': selectTabId === tab.id,
+              })}
+            >
+              <a
+                href={`#${tab.id}`}
+                onClick={() => handleClick(tab)}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
