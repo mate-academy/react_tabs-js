4ca134cea97aca1afe54f6cc55e0be5427955182
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-10-24 10:33:58 +0300

    revise Tabs component as per the PR comments

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -3,22 +3,19 @@ import cn from 'classnames';
 export const Tabs = ({ tabs, selectedTabId = 'tab-1', onTabSelected }) => {
   const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
 
-  const currentTabId = tabs.find(tab => tab.id === selectedTabId)
-    ? selectedTabId : tabs[0].id;
-
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
           {tabs.map(tab => (
             <li
-              className={cn({ 'is-active': currentTabId === tab.id })}
+              className={cn({ 'is-active': selectedTab.id === tab.id })}
               data-cy="Tab"
               key={tab.id}
             >
               <a
-                onClick={() => (currentTabId !== tab.id
-                  ? onTabSelected(tab) : '')}
+                onClick={() => (selectedTab.id !== tab.id
+                && onTabSelected(tab))}
                 href={`#${tab.id}`}
                 data-cy="TabLink"
               >
