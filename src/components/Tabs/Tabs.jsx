cde4ec11020088251c8df670389dde93b9ef3afc
Danylo Artomov <daniilart01@gmail.com>
2023-10-24 22:55:40 +0300

    implementation

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs,
+  selectedTabId,
+  setSelectedTabId,
+  onTabSelected }) => {
+  const click = (tabId, selectedId, tabsList) => {
+    if (tabId !== selectedId) {
+      onTabSelected(tabsList.find(tab => tabId === tab.id));
+    }
+  };
+
+  const getIsActive = (tabsList, tabId, selectedId) => {
+    if (tabId === selectedId
+        || tabsList.find(tab => tab.id === selectedId) === undefined) {
+      return 'is-active';
+    }
+
+    return '';
+  };
+
+  return (
+    <ul>
+      {tabs.map((tab, index) => (
+        <li
+          key={tab.id}
+          data-cy="Tab"
+          className={getIsActive(tabs, tab.id, selectedTabId)}
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={() => click(tab.id, selectedTabId, tabs)}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
