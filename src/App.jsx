bd2f3439ca8ec15e12e27c588762e05f792889f1
Zhekich123 <axelb380@gmail.com>
2023-10-26 14:44:55 +0300

    try to fix tab bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-
-export const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+import { Tabs } from './components/Tabs';
+import { tabs } from './constants';
 
 export const App = () => (
   <div className="section">
     <h1 className="title">
-      Selected tab is Tab 1
+      Selected tab is Tab
+      {tabs.id}
     </h1>
 
-    <div data-cy="TabsComponent">
+    <Tabs key={tabs.id} />
+
+    {/* <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
           <li className="is-active" data-cy="Tab">
@@ -35,6 +34,6 @@ export const App = () => (
       <div className="block" data-cy="TabContent">
         Some text 1
       </div>
-    </div>
+    </div> */}
   </div>
 );
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,43 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+import { useState } from 'react';
+import { tabs } from '../../constants';
+
+export const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(null);
+
+  const handleTabClick = (tab) => {
+    if (activeTab === tab.id) {
+      setActiveTab(null); // Если текущая вкладка уже активна, деактивируем её
+    } else {
+      setActiveTab(tab.id); // Иначе, устанавливаем новую активную вкладку
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={cn({ 'is-active': activeTab === tab.id })}
+            >
+              <a
+                href={tab.id}
+                data-cy="TabLink"
+                onClick={() => handleTabClick(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === activeTab)?.content}
+      </div>
+    </div>
+  );
+};
