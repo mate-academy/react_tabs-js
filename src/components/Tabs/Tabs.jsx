ca51bdfaaee26bcaf42782ea4f6c2ebd0617d4f5
dominosl <artemenko.mik@gmail.com>
2023-11-04 11:06:13 +0200

    fix: fixes after code review 2023-11-04 11:06:13

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -6,41 +6,46 @@ export const Tabs = ({
   onTabSelected,
 }) => {
   const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+  const { id, content } = selectedTab;
 
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
-            <li
-              className={
-                cn({
-                  'is-active': tab.id === selectedTab.id,
-                })
+          {tabs.map((tab) => {
+            function handleClick() {
+              if (tab.id === id) {
+                return;
               }
-              data-cy="Tab"
-              key={tab.id}
-            >
-              <a
-                href={`#${tab.id}`}
-                data-cy="TabLink"
-                onClick={() => {
-                  if (selectedTab.id === tab.id) {
-                    return;
-                  }
 
-                  onTabSelected(tab);
-                }}
+              onTabSelected(tab);
+            }
+
+            return (
+              <li
+                className={
+                  cn({
+                    'is-active': tab.id === id,
+                  })
+                }
+                data-cy="Tab"
+                key={tab.id}
               >
-                {tab.title}
-              </a>
-            </li>
-          ))}
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={handleClick}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-        {selectedTab.content}
+        {content}
       </div>
     </div>
   );
