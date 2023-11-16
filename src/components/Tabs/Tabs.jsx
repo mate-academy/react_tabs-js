a2bb11fd2c1bdeb276a2ebaabc3d8886d6612e16
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-11-16 16:22:05 +0100

    created tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ selectTab, allTab, byClick }) => (
+  <>
+    <div className="tabs is-boxed">
+      <ul>
+        {allTab.map(tab => (
+          <li
+            key={tab.id}
+            className={cn({
+              'is-active': selectTab === tab.id,
+            })}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => byClick(tab.id)}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {allTab.find(tab => tab.id === selectTab)?.content}
+    </div>
+  </>
+);
