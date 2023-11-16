975c57025f3410552f5d1432d6ea061d46c828a5
Nazariy Dovgyi <nazar.dovgyi@gmail.com>
2023-11-16 09:28:39 +0100

    fixes

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,32 +1,35 @@
+import React from 'react';
 import cn from 'classnames';
 
 export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
-  const validSelectedTabId = tabs.some(tab => tab.id === selectedTabId)
+  const validSelectedTabId = tabs.some(({ id }) => id === selectedTabId)
     ? selectedTabId
     : tabs[0].id;
+
+  const handleTabClick = (e, tab) => {
+    if (tab.id !== validSelectedTabId) {
+      onTabSelected(tab);
+    }
+
+    e.preventDefault();
+  };
 
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
+          {tabs.map(({ id, title, content }) => (
             <li
-              className={cn({ 'is-active': tab.id === validSelectedTabId })}
+              className={cn({ 'is-active': id === validSelectedTabId })}
               data-cy="Tab"
-              key={tab.id}
+              key={id}
             >
               <a
-                href={`#${tab.id}`}
+                href={`#${id}`}
                 data-cy="TabLink"
-                onClick={(e) => {
-                  if (tab.id !== validSelectedTabId) {
-                    onTabSelected(tab);
-                  }
-
-                  e.preventDefault();
-                }}
+                onClick={e => handleTabClick(e, { id, title, content })}
               >
-                {tab.title}
+                {title}
               </a>
             </li>
           ))}
@@ -34,7 +37,7 @@ export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
       </div>
 
       <div className="block" data-cy="TabContent">
-        {tabs.find(tab => tab.id === validSelectedTabId)?.content}
+        {tabs.find(({ id }) => id === validSelectedTabId)?.content}
       </div>
     </div>
   );
