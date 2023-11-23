5d712705c6c2eba24a748be16cac89dcb0aff721
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2023-11-23 14:01:50 +0200

    task done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const selectedId = tabs.find(tab => tab.id === selectedTabId) !== undefined
+    ? selectedTabId
+    : tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({ 'is-active': tab.id === selectedId })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={(tab.id) !== selectedId
+                  ? () => onTabSelected(tab)
+                  : undefined}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === selectedId).content}
+      </div>
+    </div>
+  );
+};
