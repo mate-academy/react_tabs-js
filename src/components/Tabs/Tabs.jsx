8b9d461724369f9e8c91f5c0fa9825660d4824f3
Svitlana <svitlana.ponomarchuk@gmail.com>
2023-11-23 21:32:47 +0200

    added tabs component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const handlerOnClick = (item) => {
+    if (selectedTabId !== item.id) {
+      onTabSelected(item);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(item => (
+            <li
+              className={cn({ 'is-active': item.id === selectedTabId.id })}
+              data-cy="Tab"
+              key={item.id}
+            >
+              <a
+                href={`#${item.id}`}
+                data-cy="TabLink"
+                onClick={() => handlerOnClick(item)}
+              >
+                {item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTabId.content}
+      </div>
+    </div>
+  );
+};
