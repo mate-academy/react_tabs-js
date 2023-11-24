b80293cf9fcb5f82ed8866ca43a406b71d08c643
Ruslan Hrynchuk <r.momos@gmail.com>
2023-11-24 20:17:42 +0200

    1 test is not passed

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,16 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-let currentTab = tabs[0];
-
 export const App = () => {
-  const [selectedTabId, setSelectedTabId] = useState(currentTab.id);
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
 
   return (
-    <div className="section">
-      <h1 className="title">
-        {`Selected tab is ${currentTab.title}`}
-      </h1>
-
-      <Tabs
-        tabs={tabs}
-        selectedTabId={selectedTabId}
-        onTabSelected={(newTab) => {
-          if (newTab.id !== currentTab.id) {
-            currentTab = newTab;
-            setSelectedTabId(newTab.id);
-          }
-        }}
-        currentContent={currentTab.content}
-      />
-    </div>
+    <Tabs
+      tabs={tabs}
+      selectedTabId={selectedTabId}
+      onTabSelected={(newTab) => {
+        setSelectedTabId(newTab.id);
+      }}
+    />
   );
 };
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -4,31 +4,46 @@ export const Tabs = ({
   tabs,
   selectedTabId,
   onTabSelected,
-  currentContent,
-}) => (
-  <div data-cy="TabsComponent">
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            className={cn({ 'is-active': tab.id === selectedTabId })}
-            data-cy="Tab"
-            key={tab.id}
-          >
-            <a
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-              onClick={() => onTabSelected(tab)}
-            >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+}) => {
+  const currentTab = tabs
+    .find(tab => tab.id === selectedTabId)
+    || tabs[0];
 
-    <div className="block" data-cy="TabContent">
-      {currentContent}
+  return (
+    <div className="section">
+      <h1 className="title">
+        {`Selected tab is ${currentTab.title}`}
+      </h1>
+
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                className={cn({ 'is-active': tab.id === selectedTabId })}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (tab.id !== selectedTabId) {
+                      onTabSelected(tab);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {currentTab.content}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
