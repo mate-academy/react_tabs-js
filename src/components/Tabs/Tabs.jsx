92799a0afd47f84cbbc41782f22d0096b05f7aa3
Fluebubble <Trots.anatoly@gmail.com>
2023-11-24 11:38:45 +0200

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,45 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = (props) => {
+  const { tabs, selectedTabId, onTabSelected } = props;
+
+  const selectedTab = tabs.find(tab => selectedTabId === tab.id) || tabs[0];
+
+  return (
+    <>
+      <div data-cy="TabsComponent">
+        <h1 className="title">
+          {`Selected tab is ${selectedTab.title}`}
+        </h1>
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                className={cn({
+                  'is-active': selectedTab.id === tab.id,
+                })}
+                key={tab.id}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (selectedTab.id !== tab.id) {
+                      onTabSelected(tab);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="block" data-cy="TabContent">
+          {selectedTab.content}
+        </div>
+      </div>
+    </>
+  );
+};
