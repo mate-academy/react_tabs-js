c063a0318c062364d60b3058ca4135f985e4eacd
Egor <yehorheneralov@gmail.com>
2023-11-25 19:14:35 +0200

    many mistakes

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+// const tabs =
+
+export const Tabs = ({ tabs, selectedTab }) => {
+  const currentTab = tabs.find(tab => tab.id === selectedTab);
+
+  return (
+    <>
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({ 'is-active': tab.id === currentTab.id })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => setSelectedTab(currentTab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currentTab.content}
+      </div>
+    </div>
+    </>
+  );
+};
