60d761c52f05653be3ed45d13bfafbf83c00279e
Alona Shelikhova <ashelyaa17@gmail.com>
2023-11-27 20:04:59 +0200

    react-tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,26 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed" data-cy="tab-content">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            className={tab.id === selectedTab ? 'is-active' : ''}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {tabs.find(tab => tab.id === selectedTab).content}
+    </div>
+  </div>
+);
