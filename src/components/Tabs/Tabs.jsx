e59cd825f8e9ef1eff8692eff8fa5b252998202b
Sasha <sashakozoriz2012@gmail.com>
2023-11-28 21:34:52 +0200

    Done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,47 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const handleTabClick = (tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
+
+  let updatedSelectedTabId = selectedTabId;
+
+  if (!tabs.some(tab => tab.id === selectedTabId)) {
+    updatedSelectedTabId = tabs[0].id;
+  }
+
+  const selectedContent = tabs.find(tab => tab.id === updatedSelectedTabId)
+    .content;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === updatedSelectedTabId ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => handleTabClick(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div
+        className="block"
+        data-cy="TabContent"
+      >
+        {selectedContent}
+      </div>
+    </div>
+  );
+};
