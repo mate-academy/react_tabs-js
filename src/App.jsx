18292298f82b7e5c8609d0e099050e58c0952c90
Misha Grinko <glukym@gmail.com>
2023-11-30 08:21:41 +0200

    finished task

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs';
 
 export const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  {
+    id: 'tab-1',
+    title: 'Tab 1',
+    content: 'Switch to 3 Tab',
+  },
+  {
+    id: 'tab-2',
+    title: 'Tab 2',
+    content: 'I\'m not here, look for me on 4 Tab',
+  },
+  {
+    id: 'tab-3',
+    title: 'Tab 3',
+    content: 'No, switch to 2 Tab',
+  },
+  {
+    id: 'tab-4',
+    title: 'Tab 4',
+    content: 'I caught you',
+  },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">
-      Selected tab is Tab 1
-    </h1>
+export const App = () => {
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+  const [onTabSelected, setSelectedTab] = useState(tabs[0].title);
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">Tab 1</a>
-          </li>
+  function updateTabId(title) {
+    tabs.forEach((tab) => {
+      if (tab.title === title) {
+        setSelectedTabId(tab.id);
+        setSelectedTab(tab.title);
+      }
+    });
+  }
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">Tab 2</a>
-          </li>
+  return (
+    <div className="section">
+      <h1 className="title">
+        {`Selected tab is ${onTabSelected}`}
+      </h1>
 
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">Tab 3</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="block" data-cy="TabContent">
-        Some text 1
-      </div>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTabId}
+        selectedTab={(title) => {
+          updateTabId(title);
+        }}
+      />
     </div>
-  </div>
-);
+  );
+};
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,65 @@
-export const Tabs = () => {};
+import React from 'react';
+import './Tabs.scss';
+import cn from 'classnames';
+
+export const Tabs = ({
+  tabs,
+  selectedTabId,
+  selectedTab,
+}) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            data-cy="Tab"
+            className={cn({ 'is-active': selectedTabId === tab.id })}
+          >
+            <a
+              href={`#${selectedTabId}`}
+              data-cy="TabLink"
+              onClick={() => selectedTab(tab.title)}
+            >
+              {selectedTab && tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div
+      className="block"
+      data-cy="TabContent"
+    >
+      <>
+        <p className="content">
+          {selectedTabId && tabs.find(tab => tab.id === selectedTabId)?.content}
+        </p>
+
+        {selectedTabId === tabs[3]?.id && (
+          <div className="surprise">
+            <p className="surprise__text">
+              Click on the picture and listen to this legendary song: `People
+              are Strange` by The Doors
+            </p>
+            {/* eslint-disable-next-line max-len */}
+            <a
+              href="https://www.youtube.com/watch?v=AgHaGrZkkv4"
+              className="surprise__link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                /* eslint-disable-next-line max-len */
+                src="https://i.ytimg.com/vi/AgHaGrZkkv4/maxresdefault.jpg"
+                alt="Album Cover"
+                className="surprise__image"
+              />
+            </a>
+          </div>
+        )}
+      </>
+    </div>
+  </div>
+);
