27188725acc67b0960f1483fff5234209b543271
Taras Niklevych <niklevychtaras@gmail.com>
2023-12-11 16:31:33 +0200

    add "Tabs" component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,19 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li className="is-active" data-cy="Tab">
+            <a href={`#${tab.id}`} data-cy="TabLink">
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      Some text 1
+    </div>
+  </div>
+);
