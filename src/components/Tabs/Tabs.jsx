5a2005333f8c34a2e9f0b79025a0f7b634fd32b5
Volodymyr Baraban <barabanv2003@gmail.com>
2023-12-18 15:09:11 +0200

    react-tab first ct

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,46 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const handleTabSelect = (tab) => {
+    if (tab.id === selectedTabId) {
+      return;
+    }
+
+    onTabSelected(tab);
+  };
+
+  const getSelectedTab =
+    tabs.find((tab) => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((e) => {
+            const { id, title } = e;
+
+            return (
+              <li
+                key={id}
+                className={cn({ 'is-active': id === getSelectedTab.id })}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleTabSelect(e)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {getSelectedTab.content}
+      </div>
+    </div>
+  );
+};
