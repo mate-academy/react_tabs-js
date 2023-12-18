c00b28c7a5613e63787b4601f380b71b1a38b175
Kriss Kozak <misskrissretro@gmail.com>
2023-12-18 23:15:12 +0200

    react_tabs-js

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,22 @@
-export const Tabs = () => {};
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => (
+  <div className="tabs is-boxed">
+    <ul>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={selectedTabId === tab.id ? 'is-active' : ''}
+          data-cy="Tab"
+        >
+          <a
+            href={`#${tab.id}`}
+            onClick={() => onTabSelected(tab)}
+            data-cy="TabLink"
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
