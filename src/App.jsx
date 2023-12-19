a7f301cd77aa8960c70c0a86301235d9e38de1df
Vadym Kondratiuk <vadim.kondrat03@gmail.com>
2023-12-19 18:17:43 +0200

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,19 @@ export const tabs = [
 ];
 
 export const App = () => {
-  const [selectedTabId, setSelectedTabId] = useState(tabs[0]);
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
 
   return (
     <div className="section">
       <h1 className="title">
-        {`Selected tab is ${selectedTabId.title}`}
+        {`Selected tab is ${selectedTab.title}`}
       </h1>
 
       <Tabs
         tabs={tabs}
-        selectedTabId={selectedTabId.id}
+        selectedTabId={selectedTab.id}
         onTabSelected={(tab) => {
-          setSelectedTabId(tab);
+          setSelectedTab(tab);
         }}
       />
     </div>
