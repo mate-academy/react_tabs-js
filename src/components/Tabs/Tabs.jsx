469a2687d19f15c2d304d8b00b4c07fbef92549f
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2023-12-23 19:11:40 +0100

    add react-tabs solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,43 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({
+  tabs,
+  onTabSelected,
+  selectedTabId,
+}) => {
+  const content = tab => (selectedTabId === tab.id ? <>{tab.content}</> : null);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn('', {
+                'is-active': tab.id === selectedTabId,
+              })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => onTabSelected(tab.id)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.map(tab => (
+          <div key={tab.id}>
+            {content(tab)}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
