c892184786fb8fc69f40421b1ee2b62f36eb3349
Roman Tarasov <rtarasov1k90@gmail.com>
2024-01-04 02:10:35 +0200

    Tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={cn({ 'is-active': tab.id === selectedTab.id })}
+            data-cy="Tab"
+          >
+            <a
+              onClick={
+                (event) => {
+                  if (tab !== selectedTab) {
+                    onTabSelected(tab);
+                  }
+                }
+              }
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {`${selectedTab.content}`}
+    </div>
+  </div>
+);
