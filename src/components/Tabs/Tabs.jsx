c83b33bdb615f4bba5f587fac09ad4032344cb19
Roman Tarasov <rtarasov1k90@gmail.com>
2024-01-04 12:43:30 +0200

    Separate function

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,35 +1,39 @@
 import cn from 'classnames';
 
-export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
-  <div data-cy="TabsComponent">
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            key={tab.id}
-            className={cn({ 'is-active': tab.id === selectedTab.id })}
-            data-cy="Tab"
-          >
-            <a
-              onClick={
-                (event) => {
-                  if (tab !== selectedTab) {
-                    onTabSelected(tab);
-                  }
-                }
-              }
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-            >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => {
+  const select = (tab) => {
+    if (tab !== selectedTab) {
+      onTabSelected(tab);
+    }
+  };
 
-    <div className="block" data-cy="TabContent">
-      {`${selectedTab.content}`}
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={cn({ 'is-active': tab.id === selectedTab.id })}
+              data-cy="Tab"
+            >
+              <a
+                onClick={
+                 () => select(tab)
+                }
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {`${selectedTab.content}`}
+      </div>
     </div>
-  </div>
-);
+  );
+};
