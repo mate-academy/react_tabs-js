f736cc847f0a33b936a4f487c4e522c324a7a9c5
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-01-04 21:58:32 +0200

    corrected code

--- src/components/Tabs/Tab.jsx
+++ src/components/Tabs/Tab.jsx
@@ -1,15 +1,20 @@
 import cn from 'classnames';
 
-export const Tab = ({ tab, activeTab, onTabChange }) => (
-  <li className={cn({ 'is-active': activeTab.id === tab.id })} data-cy="Tab">
-    <a
-      href={`#${tab.id}`}
-      data-cy="TabLink"
-      onClick={() => {
-        onTabChange(tab);
-      }}
-    >
-      {tab.title}
-    </a>
-  </li>
-);
+export const Tab = ({ tab, activeTab, onTabChange }) => {
+  const handelClick = () => {
+    onTabChange(tab);
+  };
+
+  return (
+    <li className={cn({ 'is-active': activeTab.id === tab.id })} data-cy="Tab">
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={activeTab.id !== tab.id ? handelClick : null}
+
+      >
+        {tab.title}
+      </a>
+    </li>
+  );
+};
