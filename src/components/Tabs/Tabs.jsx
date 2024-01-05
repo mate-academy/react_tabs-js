a9705f2e768e438fa92e637a6612ac6f4ab9e39f
RVDotsenko <docenkoromaha@gmail.com>
2024-01-05 16:39:14 +0200

    used an external fucntion insread of calling onClick, used a tab destruction, removed es-lint blocker

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -4,37 +4,43 @@ export const Tabs = ({
   tabs,
   selectedTab,
   onTabSelected,
-}) => (
-  <div data-cy="TabsComponent">
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          // eslint-disable-next-line
-          <li
-            className={cn({ 'is-active': selectedTab.id === tab.id })}
-            data-cy="Tab"
-            onClick={() => {
-              if (selectedTab !== tab) {
-                onTabSelected(tab);
-              }
-            }}
-            key={tab.id}
-          >
-            <a
-              id={tab.id}
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-            >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+}) => {
+  const checkTabState = (tab) => {
+    if (selectedTab !== tab) {
+      onTabSelected(tab);
+    }
+  };
 
-    <div className="block" data-cy="TabContent">
-      {selectedTab.content}
-    </div>
-  </div>
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
 
-);
+            return (
+              <li
+                className={cn({ 'is-active': selectedTab.id === id })}
+                data-cy="Tab"
+                key={id}
+              >
+                <a
+                  id={id}
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => checkTabState(tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
