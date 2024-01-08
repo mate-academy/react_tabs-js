42d29c7b4252e4b9730b8006d724113c1c2dabc8
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2024-01-08 17:52:12 +0200

    add tag ul in Tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -3,14 +3,16 @@ import { Tab } from '../Tab/Tab';
 export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
   <div data-cy="TabsComponent">
     <div className="tabs is-boxed">
-      {tabs.map(tab => (
-        <Tab
-          key={tab.id}
-          tab={tab}
-          isSelected={selectedTab === tab}
-          onTabSelected={onTabSelected}
-        />
-      ))}
+      <ul>
+        {tabs.map(tab => (
+          <Tab
+            key={tab.id}
+            tab={tab}
+            isSelected={selectedTab === tab}
+            onTabSelected={onTabSelected}
+          />
+        ))}
+      </ul>
     </div>
 
     <div className="block" data-cy="TabContent">
