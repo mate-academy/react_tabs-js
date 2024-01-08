4cd87398651d85c7e69ef76273ac2bc050299588
Nazar Malakhovskiy <malahovskinazar007@gmail.com>
2024-01-09 00:31:47 +0200

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,47 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const currTab = tabs.find(tab => tab.id === selectedTabId || tab[0]);
+
+  const handleTabClick = (isTabCurr, tab) => {
+    if (!isTabCurr) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
+            const isTabCurr = id === currTab.id;
+
+            return (
+              <li
+                className={cn({
+                  'is-actice': isTabCurr,
+                })}
+                data-cy="Tab"
+                key={id}
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() =>
+                    handleTabClick(isTabCurr, tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currTab.content}
+      </div>
+    </div>
+  );
+};
