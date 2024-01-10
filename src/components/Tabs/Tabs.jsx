0784ef301cbfd8829e8aedf4c87275b96931671a
Vlad Pashko <vladpavloggg@gmail.com>
2024-01-10 15:35:00 +0200

    added handler

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,37 +1,41 @@
 import cn from 'classnames';
 
-export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
-  <div data-cy="TabsComponent">
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map((tab) => {
-          const { id, title } = tab;
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => {
+  function handler(tab) {
+    if (tab !== selectedTab) {
+      onTabSelected(tab);
+    }
+  }
 
-          return (
-            <li
-              className={cn(tab === selectedTab && 'is-active')}
-              data-cy="Tab"
-              key={id}
-            >
-              <a
-                href={`#${id}`}
-                data-cy="TabLink"
-                onClick={() => {
-                  if (tab !== selectedTab) {
-                    onTabSelected(tab);
-                  }
-                }}
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                className={cn(tab === selectedTab && 'is-active')}
+                data-cy="Tab"
+                key={id}
               >
-                {title}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => handler(tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
-    <div className="block" data-cy="TabContent">
-      {selectedTab.content}
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
     </div>
-  </div>
-);
+  );
+};
