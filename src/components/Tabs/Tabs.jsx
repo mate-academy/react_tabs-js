0c0631a165bb55027764f8db35ac5d297c71412f
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-01-10 17:41:34 +0200

    task done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,36 @@
-export const Tabs = () => {};
+// import { tabs } from '../../App';
+
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => {
+  function handleClick(tab) {
+    if (selectedTab.id !== tab) {
+      onTabSelected(tab);
+    }
+  }
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={selectedTab.id === tab.id ? 'is-active' : ''}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => handleClick(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
