c2e86177094c636c826d4be78add7c8c991f8d9d
unknown <viktor.kniahnytskyi@gmail.com>
2024-01-10 11:51:17 +0200

    1

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map((tab) => {
+          const { id, title } = tab;
+
+          return (
+            <li
+              className={cn(tab === selectedTab && 'is-active')}
+              data-cy="Tab"
+              key={id}
+            >
+              <a
+                href={`#${id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (tab !== selectedTab) {
+                    onTabSelected(tab);
+                  }
+                }}
+              >
+                {title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
