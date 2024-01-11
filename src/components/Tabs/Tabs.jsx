0fe2036d460f6d221687766549323cd1d260ae81
Olexandr Rapin <alexrapin04@gmail.com>
2024-01-11 11:22:28 +0200

    develop

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,27 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={selectedTab === tab.id && 'is-active'}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${tab.id}`}
+              onClick={() => onTabSelected(tab)}
+              data-cy="TabLink"
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {tabs.find(tab => tab.id === selectedTab)?.content}
+    </div>
+  </div>
+);
