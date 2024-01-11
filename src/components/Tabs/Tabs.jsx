7fb6544e29f478f2ece7984c5b62d6fdd9ddd325
unknown <anorifrell@gmail.com>
2024-01-11 15:44:51 +0200

    add tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,47 @@
-export const Tabs = () => {};
+export const Tabs = ({
+  tabs,
+  selectedTab,
+  clickedTab,
+}) => {
+  function handleClick(activeTab) {
+    if (activeTab !== selectedTab) {
+      clickedTab(activeTab);
+    }
+  }
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {
+            tabs.map(tab => (
+              <li
+                key={tab.id}
+                className={
+                  selectedTab === tab
+                    ? 'is-active'
+                    : ''
+                }
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    handleClick(tab);
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
