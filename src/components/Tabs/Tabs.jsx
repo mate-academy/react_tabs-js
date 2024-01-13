8fe4c724810b9a41103f2d66bf7f1bd7994d388f
Nikita Krasun <heysleare.ls@gmail.com>
2024-01-13 18:21:04 +0200

    task solut

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,20 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTab, setSelectedTab }) => (
+  <div className="tabs is-boxed">
+    <ul>
+      {tabs.map(tab => (
+        <li
+          className={selectedTab.id === tab.id && 'is-active'}
+          data-cy="Tab"
+        >
+          <a
+            onClick={() => (setSelectedTab(tab))}
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
