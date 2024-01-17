6e0aad76c38d883a1430c3bf3fcc61943162fe95
Denys Lytvyn <ddl@lytvyn.com>
2024-01-17 16:15:19 +0200

    almost done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,43 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export function Tabs({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId)
+    || tabs[0];
+
+  const handleClick = (tab) => {
+    if (selectedTabId !== tab.id) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title, content }) => (
+            <li
+              key={id}
+              className={cn({ 'is-active': selectedTab.id === id })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${id}`}
+                data-cy="TabLink"
+                onClick={() => handleClick({ id, title, content })}
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+}
