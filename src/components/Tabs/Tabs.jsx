972d2df65fd649c66a89ed2301de6c0419495026
MrWhiteres <alexandr.leluk.99@gmail.com>
2024-01-22 22:07:42 +0200

    add task solutions

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,23 @@
-export const Tabs = () => {};
+import React from 'react';
+import { Tab } from '../Tab';
+
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <Tab
+            key={tab.id}
+            tab={tab}
+            selected={selectedTab.id === tab.id}
+            onTabSelected={onTabSelected}
+          />
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
