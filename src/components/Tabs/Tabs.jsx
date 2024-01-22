3c47ed33697d2e489beb27814d3b9e45230f0635
VolodimirKurhan <aeroafuss@mail.com>
2024-01-22 13:56:13 +0200

    sol

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,33 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            className={cn({ 'is-active': selectedTab === tab })}
+            key={tab.id}
+            data-cy="Tab"
+          >
+            <a
+              onClick={() => {
+                if (selectedTab !== tab) {
+                  onTabSelected(tab);
+                }
+              }}
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
