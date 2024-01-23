3c88a5d1ea11e3cdf6128224d6dd12c3e887e96d
Viktoriia Melnyk <melnychka1993@gmail.com>
2024-01-23 09:44:26 +0000

    done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+import '@fortawesome/fontawesome-free/css/all.css';
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, currentTab, onTabSelected }) => {
+  const handleTabClick = tab => () => {
+    if (tab.id !== currentTab.id) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({
+                'is-active': currentTab.id === tab.id,
+              })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={handleTabClick(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currentTab.content}
+      </div>
+    </div>
+  );
+};
