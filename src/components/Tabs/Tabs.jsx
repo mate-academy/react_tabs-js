fb9eb6949f8f2607109960825adc43581e88fdc6
Leonid Trotsko <leonidlikew@gmail.com>
2024-01-24 20:30:36 +0200

    fix f

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,25 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTab, onTabSelected: onTab }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            className={selectedTab.id === tab.id ? 'is-active' : null}
+            data-cy="Tab"
+          >
+            <a
+              onClick={() => selectedTab.id !== tab.id && onTab(tab)}
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
