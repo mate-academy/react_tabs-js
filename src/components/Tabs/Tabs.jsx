968d7f4308822c41bf6a1c4f2793a127f33f6419
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-11 12:02:07 +0200

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,33 @@
-export const Tabs = () => {};
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={selectedTab === tab ? 'is-active' : ''}
+            data-cy="Tab"
+          >
+            <a
+              onClick={() => {
+                if (selectedTab.id !== tab.id) {
+                  onTabSelected(tab);
+                }
+              }}
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className="block" data-cy="TabContent">
+      {selectedTab ? selectedTab.content : ''}
+    </div>
+  </div>
+);
