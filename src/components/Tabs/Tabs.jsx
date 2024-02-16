da9cd2dbd9795a523d05771ca041e5b952288261
Nadinnnka <tymoshyk02@gmail.com>
2024-02-16 16:05:27 +0200

    add key

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -4,19 +4,9 @@ export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
   <div data-cy="TabsComponent">
     <div className="tabs is-boxed">
       <ul>
-        {/* <li className="is-active" data-cy="Tab">
-          <a href="#tab-1" data-cy="TabLink">Tab 1</a>
-        </li>
-
-        <li data-cy="Tab">
-          <a href="#tab-2" data-cy="TabLink">Tab 2</a>
-        </li>
-
-        <li data-cy="Tab">
-          <a href="#tab-3" data-cy="TabLink">Tab 3</a>
-        </li> */}
         {tabs.map(tab => (
           <li
+            key={tabs.id}
             data-cy="Tab"
             className={cn({ 'is-active': tab.id === selectedTab.id })}
           >
