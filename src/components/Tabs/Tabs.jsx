f6a844d9dce439adc4ee400adb1d22f9b2b54517
unknown <vorobetst777@gmail.com>
2024-02-20 23:52:12 +0200

    v1

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,21 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, onTabClick, someID, someContent }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li key={tab.id} className={tab.id === someID ? 'is-active' : ''} data-cy="Tab">
+              <a
+                href={`#${tab.id}`}
+                onClick={() => onTabClick(tab.title, tab.id, tab.content)}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+};
