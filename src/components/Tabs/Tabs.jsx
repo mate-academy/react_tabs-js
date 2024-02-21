c994a275ff99100796dbb7eb8ededc9e566ad941
Vasya564 <dudlavasil@gmail.com>
2024-02-21 15:30:58 +0200

    Add solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,33 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => {
+          const isActive = selectedTab === tab;
+
+          return (
+            // eslint-disable-next-line
+            <li
+              className={isActive ? 'is-active' : ''}
+              data-cy="Tab"
+              key={tab.id}
+              onClick={() => {
+                if (!isActive) {
+                  onTabSelected(tab);
+                }
+              }}
+            >
+              <a href={`#${tab.id}`} data-cy="TabLink">
+                {tab.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
