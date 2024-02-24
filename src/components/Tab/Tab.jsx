0f1173b520410b3e6a3d2323b48f8c36f733b829
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2024-02-24 16:33:28 +0200

    tab is fixed

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -1,33 +1,26 @@
-import { useRef, useEffect } from 'react';
 import cn from 'classnames';
+import 'bulma/css/bulma.css';
 
 export const Tab = ({ id, title, tabActiveIdVar, changeTabFromTab }) => {
   const tabActiveIdVarFromApp = tabActiveIdVar;
-  const myRef = useRef(null);
-
-  function passId() {
-    changeTabFromTab(id);
-  }
-
-  useEffect(() => {
-    myRef.current.addEventListener('click', passId);
-
-    return () => {
-      myRef.current.removeEventListener('click', passId);
-    };
-  }, []);
 
   return (
-    <li
-      ref={myRef}
-      className={cn({
-        'is-active': id === tabActiveIdVarFromApp,
-      })}
-      data-cy="Tab"
+    <button
+      type="button"
+      onClick={() => {
+        changeTabFromTab(id);
+      }}
     >
-      <a href={`#${id}`} data-cy="TabLink">
-        {title}
-      </a>
-    </li>
+      <li
+        className={cn({
+          'is-active': id === tabActiveIdVarFromApp,
+        })}
+        data-cy="Tab"
+      >
+        <a href={`#${id}`} data-cy="TabLink">
+          {title}
+        </a>
+      </li>
+    </button>
   );
 };
