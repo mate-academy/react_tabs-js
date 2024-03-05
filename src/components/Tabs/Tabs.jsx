13d981f448c878eaf5d55cf64af15f7784f19af9
Pidsvynok <barbanihor@gmail.com>
2024-03-05 20:06:53 +0200

    Tests failed

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,23 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+import { tabs } from '../../constants';
+
+export const Tabs = ({ selectedTab, setTab }) => {
+  return (
+    <ul>
+      {tabs.map(tab => {
+        return (
+          <li
+            className={cn({
+              'is-active': selectedTab.title === tab.title,
+            })}
+            data-cy="Tab"
+          >
+            <a href="#tab-1" data-cy="TabLink" onClick={() => setTab(tab)}>
+              {tab.title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
