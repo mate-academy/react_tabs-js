d898a25e8cdceb134f99bbfca9e838d1217a9d26
Sergay Trytiak <trytik12@gmail.com>
2024-03-06 20:23:36 +0200

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,19 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTab, setActiveTab}) => {
+  return (
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li className={''} data-cy="Tab" key={tab.id}>
+            <a
+              href={tab.id}
+              data-cy="TabLink"
+              onClick={() => setActiveTab(tab)}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
