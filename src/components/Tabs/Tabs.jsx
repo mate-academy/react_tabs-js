bb5e8d7ded6fc6260c9b97b2764b4779995bea7a
Dmytro Haidash <tipocto.ckujiji@gmail.com>
2024-03-10 18:56:47 +0200

    split by components

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTab }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          <li className="is-active" data-cy="Tab">
+            <a href="#tab-1" data-cy="TabLink">
+              Tab 1
+            </a>
+          </li>
+
+          <li data-cy="Tab">
+            <a href="#tab-2" data-cy="TabLink">
+              Tab 2
+            </a>
+          </li>
+
+          <li data-cy="Tab">
+            <a href="#tab-3" data-cy="TabLink">
+              Tab 3
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
