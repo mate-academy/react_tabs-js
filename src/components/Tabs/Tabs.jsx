ed0c23857550441d125fb23ca55a905cf230f11f
Yaroslav Horodynskyi <gorodinskiyyar@gmail.com>
2024-03-10 18:26:17 +0200

    add solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,36 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, selectedTab, setSelectedTab }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(item => {
+            return (
+              <li
+                className={classNames({ 'is-active': selectedTab === item })}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${item.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (selectedTab !== item) {
+                      setSelectedTab(item);
+                    }
+                  }}
+                >
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
