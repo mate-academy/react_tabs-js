7795cba93c4dd65372b3964e7729a5538744f897
Illia <ironme530@gmail.com>
2024-03-10 21:10:28 +0200

    hotfix

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,33 +1,33 @@
 import cn from 'classnames';
 
-export const Tabs = ({ tabs, selectedTab, onTabSelected }) => {
-  const isSelected = tab => tab.id === selectedTab.id;
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => {
+          const isSelected = tab.id === selectedTab.id;
 
-  return (
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
+          return (
             <li
-              className={cn({ 'is-active': isSelected(tab) })}
+              className={cn({ 'is-active': isSelected })}
               data-cy="Tab"
               key={tab.id}
             >
               <a
                 href={`#${tab.id}`}
                 data-cy="TabLink"
-                onClick={() => (!isSelected(tab) ? onTabSelected(tab) : null)}
+                onClick={() => (!isSelected ? onTabSelected(tab) : null)}
               >
                 {tab.title}
               </a>
             </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="block" data-cy="TabContent">
-        {selectedTab.content}
-      </div>
+          );
+        })}
+      </ul>
     </div>
-  );
-};
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
