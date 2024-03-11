8b2b727e0f9b6816df8f71bd53af18db648f8cff
Hlib Kaponkin <glebkapenkin@gmail.com>
2024-03-11 23:42:48 +0200

    fix

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -2,7 +2,7 @@ export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
   <ul>
     {tabs.map(tab => (
       <li
-        {...(tab === selectedTab && { className: 'is-active' })}
+        className={tab === selectedTab && 'is-active'}
         data-cy="Tab"
         key={tab.id}
       >
