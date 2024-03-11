c3cd86a594cf0e7b3c831a28c5e6170f28fd581e
Lesia <voloshka2912@icloud.com>
2024-03-11 15:04:09 +0200

    add cn

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,26 +1,32 @@
+import cn from 'classnames';
+
 export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
   <div data-cy="TabsComponent">
     <div className="tabs is-boxed">
       <ul>
-        {tabs.map(tab => (
-          <li
-            key={tab.id}
-            className={tab === selectedTab ? 'is-active' : ''}
-            data-cy="Tab"
-          >
-            <a
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-              onClick={() => {
-                if (selectedTab !== tab) {
-                  onTabSelected(tab);
-                }
-              }}
+        {tabs.map(tab => {
+          const isAction = tab === selectedTab;
+
+          return (
+            <li
+              key={tab.id}
+              className={cn({ 'is-active': isAction })}
+              data-cy="Tab"
             >
-              {tab.title}
-            </a>
-          </li>
-        ))}
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (!isAction) {
+                    onTabSelected(tab);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
     <div className="block" data-cy="TabContent">
