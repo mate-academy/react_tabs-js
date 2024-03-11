8a44adbaafcbc1bfd1ec6c1da80e6244c0faab0f
Ihor Bahniuk <hattorikanto@gmail.com>
2024-03-11 15:13:06 +0200

    code refactoring

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,37 +1,35 @@
 import classNames from 'classnames';
 
-export const Tabs = ({ tabs, selectedTab, onTabSelected }) => {
-  return (
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => {
-            return (
-              <li
-                key={tab.id}
-                className={classNames({
-                  'is-active': tab.id === selectedTab.id,
-                })}
-                data-cy="Tab"
-              >
-                <a
-                  href={`#${tab.id}`}
-                  data-cy="TabLink"
-                  onClick={() =>
-                    tab.id !== selectedTab.id ? onTabSelected(tab) : null
-                  }
-                >
-                  {tab.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => {
+          const isSelectedTab = tab.id === selectedTab.id;
 
-      <div className="block" data-cy="TabContent">
-        {selectedTab.content}
-      </div>
+          return (
+            <li
+              key={tab.id}
+              className={classNames({
+                'is-active': isSelectedTab,
+              })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => (!isSelectedTab ? onTabSelected(tab) : null)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  );
-};
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
