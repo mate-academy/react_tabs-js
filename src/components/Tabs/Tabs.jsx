2019f59cf0995f46009a91ab431fb2cbfc0c17b1
verandetta <verandetta@yahoo.com>
2024-03-12 23:22:10 +0200

    add task solution0

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,28 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({
+  tabs,
+  selectedTab,
+  setSelectedTab,
+  message,
+  setMessage,
+}) => {
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li className={cn} data-cy="Tab">
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={() => {
+              setSelectedTab(tab.title);
+              setMessage(tab.content);
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
