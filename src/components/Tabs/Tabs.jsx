f861c29d510239cf81fba1a00e5bed950fb69a8d
Renat Abdrakhmanov <renatabdrah001@gmail.com>
2024-03-12 19:33:00 +0200

    4 tests failed

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,33 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, onTabSelected, selectedTab }) => {
+  const tabClick = title => {
+    onTabSelected(title);
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={selectedTab === tab.title ? 'is-active' : ''}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => tabClick(tab.title)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.title === selectedTab)?.content}
+      </div>
+    </div>
+  );
+};
