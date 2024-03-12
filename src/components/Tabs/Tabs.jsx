7ef3bac97feb39189d9574a6e0f774f83a00993c
Mykyta Kalyna <kalinanikita001@gmail.com>
2024-03-12 18:49:22 +0300

    solution1

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+import 'bulma/css/bulma.css';
+
+export const Tabs = ({ tabs, selectedTab, setSelectedTab }) => {
+  const handleSelected = tab => {
+    if (selectedTab.id !== tab.id) {
+      setSelectedTab(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={selectedTab.id === tab.id ? 'is-active' : ''}
+              data-cy="Tab"
+              key={tab.id}
+              onClick={() => handleSelected(tab)}
+              role="presentation"
+            >
+              <a href={`#${tab.id}`} data-cy="TabLink">
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
