bdff11c761fe1fc0fcb271147a512acb0455b55d
Maksym Prokopovych <prokopovych2006@gmail.com>
2024-03-12 14:25:24 +0200

    create-project

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,43 @@
-export const Tabs = () => {};
+import React, { useState } from 'react';
+import cn from 'classnames';
+
+export const Tabs = ({ tabs }) => {
+  const [selectedItem, setSelectedItem] = useState('tab-1');
+
+  return (
+    <>
+      <h1 className="title">
+        {tabs.find(tab => tab.id === selectedItem)
+          ? `Selected tab is ${tabs.find(tab => tab.id === selectedItem).title}`
+          : ''}
+      </h1>
+
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                key={tab.id}
+                className={cn({ 'is-active': tab.id === selectedItem })}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    setSelectedItem(tab.id);
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="block" data-cy="TabContent">
+          {tabs.find(tab => tab.id === selectedItem)?.content}
+        </div>
+      </div>
+    </>
+  );
+};
