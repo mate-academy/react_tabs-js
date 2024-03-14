d06f73ab13d13690cd0dca376a0ece9824ea17fb
DESKTOP-EHDJ2FA\yarik <yarik.zubyk@gmail.com>
2024-03-14 15:18:36 +0200

    created tabs content

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                key={tab.id}
+                className={cn({ 'is-active': selectedTab.id === tab.id })}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  onClick={() =>
+                    selectedTab.id !== tab.id && onTabSelected(tab)
+                  }
+                  data-cy="TabLink"
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.map(tab => tab.id === selectedTab.id && tab.content)}
+      </div>
+    </div>
+  );
+};
