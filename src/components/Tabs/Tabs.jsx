db5b41a70722fec0c8dd4e0c1199aeb7cc97fd3f
Oleh Marushchak <westernpc22@gmail.com>
2024-03-17 12:25:10 +0200

    add test

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,30 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, onTabSelected, selectedTab }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li className={selectedTab === tab && 'is-active'} data-cy="Tab">
+              <a
+                key={tab.id}
+                onClick={() => {
+                  if (tab !== selectedTab) {
+                    onTabSelected(tab);
+                  }
+                }}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
