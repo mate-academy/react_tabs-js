05b003a65cb5d87288436c6b8b2dd457b5487074
Misha Grinko <glukym@gmail.com>
2024-03-19 13:50:40 +0200

    fix node14 deps

--- src/components/Tabs/Tabs.spec.jsx
+++ src/components/Tabs/Tabs.spec.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-filename-extension */
-import { mount } from '@cypress/react';
+import { mount } from '@cypress/react18';
 import { Tabs } from './Tabs';
 
 const page = {
@@ -21,7 +21,7 @@ describe('Tabs component', () => {
       mount(
         <Tabs
           tabs={tabs}
-          selectedTab={tabs[1]}
+          selectedTabId="tab-20"
           onTabSelected={cy.spy().as('onTabSelected')}
         />,
       );
@@ -88,6 +88,25 @@ describe('Tabs component', () => {
       page.tabs().eq(1).find('a').click();
 
       cy.get('@onTabSelected').should('not.be.called');
+    });
+  });
+
+  describe('', () => {
+    it('should highlight the first tab if selectedTabId is wrong', () => {
+      const tabs = [
+        { id: 'tab-10', title: 'Tab 10', content: 'Some text 10' },
+        { id: 'tab-20', title: 'Tab 20', content: 'Some text 20' },
+        { id: 'tab-30', title: 'Tab 30', content: 'Some text 30' },
+        { id: 'tab-40', title: 'Tab 40', content: 'Some text 40' },
+      ];
+
+      mount(
+        <Tabs tabs={tabs} selectedTabId="asdasd" onTabSelected={() => {}} />,
+      );
+
+      page.tabs().eq(0).should('have.class', 'is-active');
+
+      page.tabs().eq(1).should('not.have.class', 'is-active');
     });
   });
 });
