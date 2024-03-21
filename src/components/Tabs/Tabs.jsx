a844f0084f2399796e6bc162c00b59e799354dfe
MaximAV <atanasovmaksim56@gmail.com>
2024-03-21 20:54:01 +0200

    version 1.0

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,30 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ selectedTab, setSelectedTab, tabs }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={classNames({ 'is-active': tab.id === selectedTab.id })}
+              data-cy="Tab"
+            >
+              <a
+                onClick={() => setSelectedTab(tab)}
+                href="#tab-1"
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
