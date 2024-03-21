40302955c40ff8509d5f22ce7a84501061c9b331
Maria Sievierova <129101728+Marys888@users.noreply.github.com>
2024-03-21 13:48:03 +0200

    add tsk solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, onTabSelected, selectedTab }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => {
+          const { id, title } = tab;
+
+          return (
+            <li
+              className={cn({ 'is-active': selectedTab.title === title })}
+              data-cy="Tab"
+              key={id}
+            >
+              <a
+                href={`#${id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (selectedTab !== tab) {
+                    onTabSelected(tab);
+                  }
+                }}
+              >
+                {title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
