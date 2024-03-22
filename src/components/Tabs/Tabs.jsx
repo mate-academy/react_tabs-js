fca7382e667cd0723d66df1866d864e7a97f4e7e
ihor_romaniuk <iromaniuk@mycubes.nl>
2024-03-22 21:45:28 +0200

    test commit

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,36 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, setActiveTabId, getTabById }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => {
+              return (
+                <li
+                  className={cn({ 'is-active': tab.id === activeTabId })}
+                  data-cy="Tab"
+                >
+                  <a
+                    href={`#${tab.id}`}
+                    data-cy="TabLink"
+                    onClick={() => {
+                      setActiveTabId(tab.id);
+                    }}
+                  >
+                    {tab.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {getTabById(activeTabId).content}
+      </div>
+    </div>
+  );
+};
