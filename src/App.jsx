fb56635e1295dff83f7076038b2610a1b85aaea2
Bohdan Shyrii <shyrii11bohdan@gmail.com>
2024-03-23 16:27:17 +0100

    farmed

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs';
 
-export const tabs = [
+export const tabsFromServer = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+export const App = () => {
+  const selectedTab = [...tabsFromServer];
+  const [activeTab, setActiveTab] = useState(selectedTab[0].id);
+  const currTabInfo = tabsFromServer.find(tab => tab.id === activeTab);
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="block" data-cy="TabContent">
-        Some text 1
+  return (
+    <div className="section">
+      <h1 className="title">{`Selected tab is ${currTabInfo.title}`}</h1>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <Tabs
+            tabs={selectedTab}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+        </div>
+        <div className="block" data-cy="TabContent">
+          {currTabInfo.content}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,27 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <ul>
+      {tabs.map(tab => {
+        return (
+          <li
+            key={tab.id}
+            data-cy="Tab"
+            className={cn({ 'is-active': activeTab === tab.id })}
+          >
+            <a
+              data-cy="TabLink"
+              href={`#${tab.id}`}
+              onClick={() => {
+                setActiveTab(tab.id);
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
