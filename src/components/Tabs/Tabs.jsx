a914f5aa34edbdfead3336035b8971305e2a4fd4
Max Petrashishin <rikogjdefolt@gmail.com>
2024-03-25 00:00:51 +0200

    solution tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const usedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  const isUsedThis = tab => {
+    if (tab.id !== usedTab.id) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({ 'is-active': usedTab.id === tab.id })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => isUsedThis(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {usedTab.content}
+      </div>
+    </div>
+  );
+};
