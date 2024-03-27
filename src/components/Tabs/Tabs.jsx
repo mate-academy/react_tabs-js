238707544d197b3941d592efab73eee60a43b12f
Radymyr <loboda.radymyr@gmail.com>
2024-03-27 18:53:25 +0200

    first commit

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,36 @@
-export const Tabs = () => {};
+export const Tabs = ({ selectedTab, onTabSelected, tabs }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { title, id } = tab;
+
+            return (
+              <li
+                key={id}
+                className={`${selectedTab.id === id && 'is-active'}`}
+                data-cy="Tab"
+              >
+                <a
+                  onClick={() => {
+                    onTabSelected(tab || tabs[0]);
+                    console.log(tab);
+                  }}
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
