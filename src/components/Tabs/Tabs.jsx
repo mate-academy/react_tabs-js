17ee02bc85068a6ee5425cab5af322acbc72d0ff
Iryna Liashko <ira.liashko1@gmail.com>
2024-03-30 16:58:28 +0100

    Almost done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,28 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => {
+  return (
+    <React.Fragment>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={tab.id === selectedTab.id ? 'is-active' : ''}
+          data-cy="Tab"
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={e => {
+              e.preventDefault();
+              onTabSelected(tab);
+            }}
+            role="tab"
+            tabIndex={0}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </React.Fragment>
+  );
+};
