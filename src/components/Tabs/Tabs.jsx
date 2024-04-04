7c121ad8a039d96680d13d0c7842470ee89efdaa
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-04-04 13:03:02 +0300

    done project

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,20 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => {
+  const isSelectedTabNotFound = !tabs.some(tab => tab.id === `tab-${selectedTab}`);
+
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          className={(isSelectedTabNotFound && tab.id === 'tab-1') || tab.id.slice(-1) === selectedTab ? 'is-active' : ''}
+          onClick={() => onTabSelected(tab.id.slice(-1))}
+          key={tab.id}
+          data-cy="Tab"
+        >
+          <a href={`#${tab.id}`} data-cy="TabLink">
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
