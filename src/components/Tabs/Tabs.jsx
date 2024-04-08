1970e9ff5cd7a1597e65fa87be3c98390bf5cd5b
Oleksandr Poviakalo <oleksandr.poviakalo@gmail.com>
2024-04-08 15:45:11 +0300

    Task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,29 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTab, onTabSelected }) => {
+  return (
+    <ul>
+      {tabs.map(tab => {
+        return (
+          <li
+            key={tab.id}
+            className={cn({ 'is-active': selectedTab.id === tab.id })}
+            data-cy="Tab"
+          >
+            <a
+              onClick={() => {
+                if (selectedTab.id !== tab.id) {
+                  onTabSelected(tab);
+                }
+              }}
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+            >
+              {tab.title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
