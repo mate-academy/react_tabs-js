24f7b42396b5f2f3fe35b5a5ee914bf669855df3
Olena Tsehelnyk <116718952+AlyonaTsehelnyk@users.noreply.github.com>
2024-04-19 22:07:54 +0300

    Solution 2

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export const App = () => {
       <h1 className="title">Selected tab is {selectedTab.title}</h1>
       <Tabs
         tabs={tabs}
-        selectedTabId={selectedTab.id}
+        activeTabId={selectedTab.id}
         onTabSelected={setSelectedTab}
       />
     </div>
