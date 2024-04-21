8d9cfcdbd125cdfc62c97929492c513b8999a046
Denys Lysiak <lisyak99.dl@gmail.com>
2024-04-21 13:57:16 +0300

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,52 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  let validId = activeTabId;
+  const arrayOfId = [];
+
+  tabs.forEach(tab => {
+    arrayOfId.push(tab.id);
+  });
+
+  if (!arrayOfId.includes(validId)) {
+    validId = tabs[0].id;
+  }
+
+  const listOfTabs = tabs.map(tab => (
+    <li
+      data-cy="Tab"
+      className={cn({ 'is-active': tab.id === validId })}
+      key={tab.id}
+    >
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={() => {
+          if (validId !== tab.id) {
+            onTabSelected(tab.id);
+          }
+        }}
+      >
+        {tab.title}
+      </a>
+    </li>
+  ));
+
+  const activeTabContent = activeId => {
+    return tabs.map(tab => {
+      return tab.id === validId ? tab.content : '';
+    });
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>{listOfTabs}</ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTabContent(validId)}
+      </div>
+    </div>
+  );
+};
