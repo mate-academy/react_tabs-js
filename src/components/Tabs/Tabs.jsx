9e98a90cd669e5b0e652e4ce791544e70ae0b303
Marian <getmoodyup@gmail.com>
2024-04-28 13:39:27 +0300

    tabs are ready

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,29 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTab, onTabSelected }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab, position) => (
+            <li
+              key={tab.id}
+              className={tab.id === activeTab ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={tab.id}
+                data-cy="TabLink"
+                onClick={event => onTabSelected(event, tab.id)}
+              >
+                {`Tab ${position + 1}`}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === activeTab)?.content}
+      </div>
+    </div>
+  );
+};
