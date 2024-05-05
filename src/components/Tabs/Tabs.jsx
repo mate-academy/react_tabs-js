bfc2306cdddf02fa689f51890cd36b52b380b0b1
Dima Komisarov <komissarov854@gmail.com>
2024-05-05 16:56:07 +0300

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -2,9 +2,10 @@ import cn from 'classnames';
 
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
-  const click = id => {
-    if (activeTabId !== id) {
-      onTabSelected(id);
+  const { id, content } = activeTab;
+  const click = tab => {
+    if (tab.id !== activeTabId) {
+      onTabSelected(tab.id);
     }
   };
 
@@ -14,24 +15,23 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
         <ul>
           {tabs.map(tab => (
             <li
-              className={cn({ 'is-active': activeTab.id === tab.id })}
+              className={cn({ 'is-active': tab.id === id })}
               data-cy="Tab"
               key={tab.id}
             >
               <a
                 href={`#${tab.id}`}
                 data-cy="TabLink"
-                onClick={() => click(tab.id)}
+                onClick={() => click(tab)}
               >
                 {tab.title}
               </a>
             </li>
           ))}
         </ul>
-
-        <div className="block" data-cy="TabContent">
-          {activeTab.content}
-        </div>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {content}
       </div>
     </div>
   );
