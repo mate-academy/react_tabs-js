ae4e64bacc8fc40090dd6108b17b3e3f79932812
Hryniuk_Taras <gritaras484@gmail.com>
2024-05-07 13:53:48 +0300

    react tabs. 2 errors

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,32 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const handleTabClick = event => {
+    const tabId = event.target.getAttribute('data-id');
+
+    if (tabId !== activeTabId) {
+      onTabSelected(tabId);
+    }
+  };
+
+  return (
+    <ul>
+      {tabs.map(tab => {
+        return (
+          <li
+            key={tab.id}
+            data-cy="Tab"
+            className={tab.id === activeTabId ? 'is-active' : ''}
+          >
+            <a
+              href={`#${tab.id}`}
+              data-id={tab.id}
+              data-cy="TabLink"
+              onClick={handleTabClick}
+            >
+              {tab.title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
