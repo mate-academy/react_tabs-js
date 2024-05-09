aeed555b52ec1ca2d6d1f5424d65f28accd87cf2
unknown <kazansasha10@gmail.com>
2024-05-09 14:50:25 +0300

    completed

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,32 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const setByClick = tabId => {
+    if (tabId !== activeTabId) {
+      onTabSelected(tabId);
+    }
+  };
+
+  return (
+    <div>
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={cn({ 'is-active': tab.id === activeTabId })}
+            data-cy="Tab"
+            onClick={() => setByClick(tab.id)}
+            aria-hidden="true"
+          >
+            <a href={`#${tab.id}`} data-cy="TabLink">
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === activeTabId)?.content}
+      </div>
+    </div>
+  );
+};
