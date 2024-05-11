0169c97119387ebb963e0881d66651e693d0b1ef
Denys Povh <kirilloffdanza@gmail.com>
2024-05-11 09:07:01 +0300

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const handleTabClick = tabId => {
+    if (tabId !== activeTabId) {
+      onTabSelected(tabId);
+    }
+  };
+
+  const renderTabLinks = () => {
+    return tabs.map(tab => (
+      <li
+        key={tab.id}
+        className={tab.id === activeTabId ? 'is-active' : ''}
+        data-cy="Tab"
+      >
+        <a
+          href={`#${tab.id}`}
+          data-cy="TabLink"
+          onClick={() => handleTabClick(tab.id)}
+          aria-current={tab.id === activeTabId ? 'page' : null}
+        >
+          {tab.title}
+        </a>
+      </li>
+    ));
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>{renderTabLinks()}</ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === activeTabId)?.content}
+      </div>
+    </div>
+  );
+};
