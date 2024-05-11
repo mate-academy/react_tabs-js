2737409ed5ec20be0f277b6b3016827f5b74c1d0
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2024-05-11 22:28:01 +0300

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,62 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tab, selectedTab, setSelectedTab }) => {
+  const { content } = tab;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          <li
+            className={classNames({ 'is-active': selectedTab === 'tab-1' })}
+            data-cy="Tab"
+          >
+            <a
+              href="#tab-1"
+              data-cy="TabLink"
+              onClick={() => {
+                setSelectedTab('tab-1');
+              }}
+            >
+              Tab 1
+            </a>
+          </li>
+
+          <li
+            className={classNames({ 'is-active': selectedTab === 'tab-2' })}
+            data-cy="Tab"
+          >
+            <a
+              href="#tab-2"
+              data-cy="TabLink"
+              onClick={() => {
+                setSelectedTab('tab-2');
+              }}
+            >
+              Tab 2
+            </a>
+          </li>
+
+          <li
+            className={classNames({ 'is-active': selectedTab === 'tab-3' })}
+            data-cy="Tab"
+          >
+            <a
+              href="#tab-3"
+              data-cy="TabLink"
+              onClick={() => {
+                setSelectedTab('tab-3');
+              }}
+            >
+              Tab 3
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {content}
+      </div>
+    </div>
+  );
+};
