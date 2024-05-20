ef58f05fc36e5c77587f53f848d66bae6b42ce1f
Anton Polishchuk <hardixxxxx@gmail.com>
2024-05-20 16:50:17 +0200

    Implemented the `Tabs` component that renders several tabs and allows the user to switch between them

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) ?? tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const TITLE = tab.title;
+            const ID = tab.id;
+
+            return (
+              <li
+                key={ID}
+                data-cy="Tab"
+                className={cn({
+                  'is-active': ID === activeTab.id,
+                })}
+              >
+                <a
+                  href={`#${ID}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (ID !== activeTab.id) {
+                      onTabSelected(ID);
+                    }
+                  }}
+                >
+                  {TITLE}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
