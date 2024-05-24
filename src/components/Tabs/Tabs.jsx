1e83c1d37f77be89866cbccf053db12dc05bf45c
SrTrace <levashov04sergey@gmail.com>
2024-05-24 08:59:13 +0300

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  if (!tabs.find(tab => tab.id === activeTabId)) {
+    // eslint-disable-next-line no-param-reassign
+    activeTabId = tabs[0].id;
+  }
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+            <li
+              key={tab.id}
+              className={cn({ 'is-active': activeTabId === tab.id })}
+              data-cy="Tab"
+              onClick={() => {
+                if (tab.id !== activeTabId) {
+                  return onTabSelected(tab.id);
+                }
+
+                return false;
+              }}
+            >
+              <a href={`#${tab.id}`} data-cy="TabLink">
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === activeTabId).content}
+      </div>
+    </div>
+  );
+};
