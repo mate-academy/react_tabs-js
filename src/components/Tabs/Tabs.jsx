9832d1ce09d4e99bccbbc581b0f85b5b5a913c53
unknown <beliyandr@gmail.com>
2024-05-24 22:58:47 +0300

    first variant

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,42 @@
-export const Tabs = () => {};
+export const Tabs = ({ activeTabId, tabs, onTabSelected }) => {
+  if (!tabs.findIndex(tab => tab.id === activeTabId < 0)) {
+    onTabSelected(tabs[0].id);
+  }
+  const content = tabs.find(tab => tab.id === activeTabId).content;
+
+  console.log(activeTabId);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                className={tab.id === activeTabId ? 'is-active' : null}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={event => {
+                    if (!event.target.href.includes(activeTabId)) {
+                      onTabSelected(tab.id);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {content}
+      </div>
+    </div>
+  );
+};
