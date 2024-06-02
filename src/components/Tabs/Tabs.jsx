17b18bd82587b8c02330341508667489fb0951ae
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-06-02 21:50:43 +0300

    solutiob

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId);
+  const { id: selectedTabId, content } = activeTab || tabs[0];
+
+  const handleSelectedTab = tabId => {
+    if (tabId !== activeTabId) {
+      onTabSelected(tabId);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }) => (
+            <li
+              key={id}
+              className={classNames({ 'is-active': id === selectedTabId })}
+              data-cy="Tab"
+            >
+              <a
+                onClick={() => handleSelectedTab(id)}
+                href={`#${id}`}
+                data-cy="TabLink"
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {content}
+      </div>
+    </div>
+  );
+};
