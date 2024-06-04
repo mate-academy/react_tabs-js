d4a91e6085750465d07a97cba83d8c19ee5eec3a
Den <clixqqk@gmail.com>
2024-06-04 16:33:20 +0300

    pull

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,48 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  // Находим активную вкладку, по умолчанию выбираем первую вкладку, если activeTabId неверен
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  // Если activeTabId неверен, уведомляем родительский компонент о выборе первой вкладки
+  if (!tabs.some(tab => tab.id === activeTabId)) {
+    onTabSelected(activeTab.id);
+  }
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={cn({
+                'is-active': activeTab.id === tab.id,
+              })}
+              data-cy="Tab"
+            >
+              <a
+                onClick={e => {
+                  e.preventDefault();
+                  // Проверяем, отличается ли выбранная вкладка от текущей активной вкладки
+                  if (activeTab.id !== tab.id) {
+                    // Вызываем функцию обратного вызова, чтобы уведомить родительский компонент
+                    onTabSelected(tab.id);
+                  }
+                }}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
