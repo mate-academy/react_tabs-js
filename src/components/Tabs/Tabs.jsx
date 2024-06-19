9a7faa854c40915e524fa17ea076720e4a5dc60b
priemssonia <priemssonia@gmail.com>
2024-06-19 15:23:14 +0300

    Add solution for Tabs task of React

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId);
+  const { id: selectedTabId, content } = activeTab || tabs[0];
+
+  const handleSelectedTab = tabId => {
+    if (tabId !== activeTabId) {
+      onTabSelected(tabId);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }) => (
+            <li
+              key={id}
+              className={cn({ 'is-active': id === selectedTabId })}
+              data-cy="Tab"
+            >
+              <a
+                onClick={() => handleSelectedTab(id)}
+                href={`#${id}`}
+                data-cy="TabLink"
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {content}
+      </div>
+    </div>
+  );
+};
