9d6c2e2410d576fc71f87dcf1e0f6d182c0da20f
natalagoncaruk <honcharukna@gmail.com>
2024-06-26 17:49:29 +0200

    Correct mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,14 @@ export const tabs = [
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState(tabs[0].id);
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
-  
+
   return (
     <div className="section">
       <h1 className="title">Selected tab is {activeTab.title}</h1>
       <Tabs
         tabs={tabs}
         activeTabId={activeTabId}
-        onTabSelected={id => setActiveTabId(id)}
+        onTabSelected={setActiveTabId}
       />
     </div>
   );
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,3 +1,5 @@
+import cn from 'classnames';
+
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
@@ -14,13 +16,12 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
           {tabs.map(({ id, title }) => (
             <li
               key={id}
-              className={id === activeTab.id ? 'is-active' : ''}
+              className={cn({ 'is-active': id === activeTab.id })}
               data-cy="Tab"
             >
               <a
                 href={`#${id}`}
-                onClick={() => {
-                  handleTabSelected(id);
+                onClick={() => {handleTabSelected(id);
                 }}
                 data-cy="TabLink"
               >
@@ -32,7 +33,7 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
       </div>
 
       <div className="block" data-cy="TabContent">
-        {activeTab ? activeTab.content : ''}
+        {activeTab.content}
       </div>
     </div>
   );
