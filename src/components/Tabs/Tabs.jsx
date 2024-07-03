e49fc83c05901c84279e6130fdf9bac1064c251a
Maksym <max2112gereta@gmail.com>
2024-07-03 17:52:00 -0400

    new commit

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+import React from 'react';
+
+const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const handleTabClick = tabId => {
+    if (tabId !== activeTabId && onTabSelected) {
+      onTabSelected(tabId);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === activeTabId ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => handleTabClick(tab.id)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === activeTabId)?.content}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
