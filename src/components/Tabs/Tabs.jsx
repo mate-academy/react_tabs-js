8076705268ca4530a1de43df60e4ff697a47c787
ZakZh <zak.zh@ukr.net>
2024-07-11 10:02:12 +0300

    update Tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -3,27 +3,29 @@ import cn from 'classnames';
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
-  function handleTabClick(e) {
-    onTabSelected(e.target.id);
-  }
+  const handleTabClick = id => {
+    if (id !== activeTabId) {
+      onTabSelected(id);
+    }
+  };
 
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
+          {tabs.map(({ id, title }) => (
             <li
-              key={tab.id}
-              className={cn({ 'is-active': tab.id === activeTab.id })}
+              key={id}
+              className={cn({ 'is-active': id === activeTab.id })}
               data-cy="Tab"
             >
               <a
-                id={tab.id}
-                href={`#${tab.id}`}
+                id={id}
+                href={`#${id}`}
                 data-cy="TabLink"
-                onClick={tab.id !== activeTabId ? handleTabClick : undefined}
+                onClick={() => handleTabClick(id)}
               >
-                {tab.title}
+                {title}
               </a>
             </li>
           ))}
