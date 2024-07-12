dd78ae946191c0f5719e370bc2612461ff9af43c
Bohdan Smachylo <smachyloooo@gmail.com>
2024-07-13 01:56:00 +0200

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { tabs } from './api/tabs';
+import { Tabs } from './components/Tabs/Tabs';
 
-export const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+function getAllTabs(datas, { currentTab }) {
+  const newDatas = datas.map(newData => {
+    if (currentTab === newData.title) {
+      return { ...newData, isActive: true };
+    }
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+    return { ...newData, isActive: false };
+  });
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
+  return newDatas;
+}
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
+export const App = () => {
+  const [currentTab, setCurrentTab] = useState(tabs[0].title);
+  const newTabs = getAllTabs(tabs, { currentTab });
 
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
-        </ul>
-      </div>
+  return (
+    <div className="section">
+      <h1 className="title">Selected tab is {currentTab}</h1>
 
-      <div className="block" data-cy="TabContent">
-        Some text 1
-      </div>
+      <Tabs newTabs={newTabs} setCurrentTab={setCurrentTab} />
     </div>
-  </div>
-);
+  );
+};
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,41 @@
-export const Tabs = () => {};
+import { useState } from 'react';
+import { tabs } from '../../api/tabs';
+
+export const Tabs = ({ newTabs, setCurrentTab }) => {
+  const [message, setMessage] = useState(tabs[0].content);
+
+  const handleClick = tab => {
+    setMessage(tab.content);
+    setCurrentTab(tab.title);
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {newTabs.map(tab => {
+            return (
+              <li
+                className={tab.isActive ? 'is-active' : ''}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  onClick={() => handleClick(tab)}
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {message}
+      </div>
+    </div>
+  );
+};
