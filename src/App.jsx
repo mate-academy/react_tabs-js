c282620c989d93cbe854d4865c9854b6f9780d37
Alexandr Shatskyy <autome@ukr.net>
2024-07-20 09:42:15 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,9 @@ export const tabs = [
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
-
 export const App = () => {
-  const activeTab = tabs.find(tab => tab.id === tabSelected) || tabs[0];
   const [tabSelected, onTabSelected] = useState(tabs[0].id);
+  const activeTab = tabs.find(tab => tab.id === tabSelected) || tabs[0];
 
   return (
     <div className="section">
