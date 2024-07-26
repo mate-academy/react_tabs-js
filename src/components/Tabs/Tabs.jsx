9f93d9354d5bfe8a95591da32a6875c2ffcd1175
pryslipskyi.d <denys.pryslipskyi@nixs.com>
2024-07-26 15:16:36 +0200

    sol

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const handleTabClick = tabId => {
+    if (tabId !== activeTabId) {
+      onTabSelected(tabId);
+    }
+  };
+
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={classNames({ 'is-active': tab.id === activeTab.id })}
+              data-cy="Tab"
+            >
+              <a
+                onClick={() => handleTabClick(tab.id)}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
