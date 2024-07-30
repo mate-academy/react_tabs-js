4c5f36e5ac06d297f3ff18846d62b23d1e54a8d8
Vitor Hugo <vitorhugovieirabraga@gmail.com>
2024-07-30 17:46:00 -0300

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTabName = tabs.find(({ id }) => activeTabId === id) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }) => (
+            <li
+              className={id === activeTabName.id ? 'is-active' : ''}
+              data-cy="Tab"
+              key={id}
+            >
+              <a
+                href={`#${id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (id !== activeTabName.id) {
+                    onTabSelected(id);
+                  }
+                }}
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTabName.content}
+      </div>
+    </div>
+  );
+};
