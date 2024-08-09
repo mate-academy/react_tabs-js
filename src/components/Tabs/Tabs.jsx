773494d3f8ec904a1ad147070ec78b0c6d6beb46
Vadym Babisov <vadim.babisov@gmail.com>
2024-08-09 11:42:25 +0300

    small fix

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -2,33 +2,34 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            className={tab.id === activeTab.id ? 'is-active' : ''}
-            data-cy="Tab"
-            key={tab.id}
-          >
-            <a
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-              onClick={event => {
-                event.preventDefault();
-                if (tab.id !== activeTab.id) {
-                  onTabSelected(tab.id);
-                }
-              }}
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={tab.id === activeTab.id ? 'is-active' : ''}
+              data-cy="Tab"
+              key={tab.id}
             >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={event => {
+                  event.preventDefault();
+                  if (tab.id !== activeTab.id) {
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className="block" data-cy="TabContent">
         {activeTab.content}
       </div>
-    </div>
+    </>
   );
 };
