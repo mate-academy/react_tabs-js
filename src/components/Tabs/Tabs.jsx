a8948d08c93e8bb421249f6de3946e499b217a92
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-08-15 08:42:57 +0300

    react_tabs-js

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,34 @@
-export const Tabs = () => {};
+import classNames from "classnames";
+export const Tabs = ({ tabs, activeTabId, onTabSelected}) => {
+  const validActiveTabId = tabs.some(tab => tab.id === activeTabId) ? activeTabId : tabs[0].id;
+
+  return (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li 
+          className={classNames({"is-active": validActiveTabId === tab.id })} 
+          data-cy="Tab"
+          >
+            <a 
+            href={`#${tab.id}`} 
+            data-cy="TabLink"
+            onClick={()=>{
+              if(tab.id !== validActiveTabId){
+                onTabSelected(tab.id)
+              }
+            }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {tabs.find(tab=>tab.id===validActiveTabId)?.content}
+    </div>
+  </div>
+)};
