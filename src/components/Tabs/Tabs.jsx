2ec5eb2547f9fcfd95b95c1a40f929f30973bd4f
stan0men@yahoo.com <stan0men@yahoo.com>
2024-08-31 20:02:33 +0300

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,51 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  const handleTabClick = tabId => {
+    if (tabId !== activeTab.id) {
+      onTabSelected(tabId);
+    }
+  };
+
+  const handleKeyDown = (event, id) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      onTabSelected(id);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const isActive =
+              tab.id === activeTabId ||
+              (activeTabId &&
+                !tabs.find(t => t.id === activeTabId) &&
+                tab.id === tabs[0].id);
+
+            return (
+              <li
+                key={tab.id}
+                data-cy="Tab"
+                tabIndex="0"
+                role="tab"
+                className={isActive ? 'is-active' : ''}
+                onClick={() => handleTabClick(tab.id)}
+                onKeyDown={event => handleKeyDown(event, tab.id)}
+              >
+                <a href={`#${tab.id}`} data-cy="TabLink">
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
