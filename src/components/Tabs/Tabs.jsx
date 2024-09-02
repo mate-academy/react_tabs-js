29a69b6073a11d992fe53ac61ac75d77f022539d
Kontroler444 <kontroler444@gmail.com>
2024-09-02 13:36:57 +0300

    Create React tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  const setOnClick = tabId => {
+    if (tabId !== activeTab.id) {
+      onTabSelected(tabId);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }) => (
+            <li
+              key={id}
+              className={cn({ 'is-active': id === activeTab.id })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${id}`}
+                onClick={() => setOnClick(id)}
+                data-cy="TabLink"
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
