c3a0df8b7c481b809f80dc16f3798a6d2134b1b3
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-09-04 11:27:02 +0300

    task done?

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,26 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, tabSelect, onTabSelected }) => {
+  return (
+    <>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          data-cy="Tab"
+          className={tabSelect.id === tab.id ? 'is-active' : ''}
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={e => {
+              e.preventDefault();
+              if (tabSelect.id !== tab.id) {
+                onTabSelected(tab);
+              }
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </>
+  );
+};
