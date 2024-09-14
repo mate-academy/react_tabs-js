ade1cd62282a99423a6da1b2375e6c979a5dc670
Anna Maksai <annamaksai99@gmail.com>
2024-09-14 10:57:03 +0300

    feat: tab selection

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(({ id }) => id === activeTabId) || tabs[0];
+
+  const handleClick = targetId => {
+    if (targetId !== activeTab.id) {
+      onTabSelected(targetId);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }) => (
+            <li
+              data-cy="Tab"
+              key={id}
+              className={cn({ 'is-active': id === activeTab.id })}
+            >
+              <a
+                href={`#${id}`}
+                data-cy="TabLink"
+                onClick={() => handleClick(id)}
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
