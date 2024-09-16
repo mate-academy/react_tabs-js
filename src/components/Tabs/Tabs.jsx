61cde44773534466fcb8f539476d4cacf0707f50
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-09-16 16:30:24 +0200

    feat(App, Tabs): add solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,28 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ activeTabId, tabs, onTabSelected }) => {
+  const activeTab =
+    activeTabId && tabs.find(tab => tab.id === activeTabId)
+      ? activeTabId
+      : tabs[0].id;
+
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          data-cy="Tab"
+          className={cn({ 'is-active': activeTab === tab.id })}
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={activeTab !== tab.id && onTabSelected(tab.id)}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
