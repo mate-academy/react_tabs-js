3a03a2be4f68139dc182ba29191e5b030b7ffccb
Yevhenii Melnyk <rpm.steel@gmail.com>
2024-09-23 19:21:03 +0300

    solved

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,32 @@
-export const Tabs = () => {};
+export function Tabs({ tabs, handleTabId, currentTab }) {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={currentTab.id === tab.id ? 'is-active' : ''}
+              key={tab.id}
+              data-cy="Tab"
+            >
+              <a
+                onClick={id => {
+                  if (id !== currentTab.id) {
+                    handleTabId(tab.id);
+                  }
+                }}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {currentTab.content}
+      </div>
+    </div>
+  );
+}
