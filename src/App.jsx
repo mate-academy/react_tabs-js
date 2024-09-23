5b6d33d4bcc2a69c81cd5351e1bcfc9f4a147866
Valerii Omelnyk <valeraaomelnik@gmail.com>
2024-09-23 15:20:46 +0300

    fix solution

--- src/App.jsx
+++ src/App.jsx
@@ -10,35 +10,21 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const getActiveTab = (inputTabs, { activeTabId }) => {
-  const activeTab = inputTabs.find(tab => tab.id === activeTabId);
-
-  return activeTab;
-};
-
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState(tabs[0].id);
 
-  const visibleTab = getActiveTab(tabs, { activeTabId });
+  const activeTab = tabs.find(tab => tab.id === activeTabId);
 
   return (
     <div className="section">
-      <h1 className="title">Selected tab is {visibleTab.title}</h1>
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
 
       <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            <Tabs
-              tabs={tabs}
-              activeTabId={activeTabId}
-              onTabSelected={id => setActiveTabId(id)}
-            />
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          {visibleTab.content}
-        </div>
+        <Tabs
+          tabs={tabs}
+          activeTabId={activeTabId}
+          onTabSelected={id => setActiveTabId(id)}
+        />
       </div>
     </div>
   );
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,23 +1,45 @@
 import cn from 'classnames';
 
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
-  return tabs.map(tab => {
-    const { id, title } = tab;
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
-    const isActive = activeTabId === id;
+  const isActive = id => id === activeTab.id;
 
-    const handleOnTabSelected = () => {
-      if (!isActive) {
-        onTabSelected(id);
-      }
-    };
+  const handleOnTabSelected = id => {
+    if (!isActive(id)) {
+      onTabSelected(id);
+    }
+  };
 
-    return (
-      <li className={cn({ 'is-active': isActive })} data-cy="Tab">
-        <a href={`#${id}`} data-cy="TabLink" onClick={handleOnTabSelected}>
-          {title}
-        </a>
-      </li>
-    );
-  });
+  return (
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                className={cn({ 'is-active': isActive(id) })}
+                data-cy="Tab"
+                key={id}
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleOnTabSelected(id)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </>
+  );
 };
