9c3415fc5f2801413053271cb4ada70906f9c914
Yevhenii Melnyk <rpm.steel@gmail.com>
2024-09-25 16:38:26 +0300

    another correction

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,9 +1,17 @@
+import cn from 'classnames';
+
 export function Tabs({ tabs, onTabSelected, activeTabId }) {
   const findTab = array => {
     return array.find(element => element.id === activeTabId);
   };
 
   const currentTab = findTab(tabs) || tabs[0];
+
+  const handleOnTabSelected = tab => {
+    if (tab.id !== currentTab.id) {
+      onTabSelected(tab.id);
+    }
+  };
 
   return (
     <>
@@ -14,16 +22,12 @@ export function Tabs({ tabs, onTabSelected, activeTabId }) {
           <ul>
             {tabs.map(tab => (
               <li
-                className={currentTab.id === tab.id ? 'is-active' : ''}
+                className={cn({ 'is-active': currentTab.id === tab.id })}
                 key={tab.id}
                 data-cy="Tab"
               >
                 <a
-                  onClick={() => {
-                    if (tab.id !== currentTab.id) {
-                      onTabSelected(tab.id);
-                    }
-                  }}
+                  onClick={() => handleOnTabSelected(tab)}
                   href={`#${tab.id}`}
                   data-cy="TabLink"
                 >
