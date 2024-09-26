31f62c299a99ec8cc54deeb4df8ea60d8de651ef
vikshiyan <miss.shiyan@gmail.com>
2024-09-26 21:17:25 +0300

    fix h1

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export const App = () => {
 
   return (
     <div className="section">
-      <h1 className="title">{`Selected tab is ${activeTab.title}`}</h1>
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
       <Tabs
         tabs={tabs}
         activeTabId={activeTabId}
