83965ba96d00993f2dd6d8b15e6517b2bdc70304
Andrii Tkachuk <kovklad@gmail.com>
2024-12-12 15:32:53 +0100

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
+// export const tabs = [
+//   { id: 'tab-10', title: 'Tab 10', content: 'Some text 10' },
+//   { id: 'tab-20', title: 'Tab 20', content: 'Some text 20' },
+//   { id: 'tab-30', title: 'Tab 30', content: 'Some text 30' },
+//   { id: 'tab-40', title: 'Tab 40', content: 'Some text 40' },
+// ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+export const App = () => {
+  const [activeTabId, setActiveTabId] = useState('');
+  const activeTab = tabs.find(el => el.id === activeTabId) || tabs[0];
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="block" data-cy="TabContent">
-        Some text 1
-      </div>
+  return (
+    <div className="section">
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
+      <Tabs
+        tabs={tabs}
+        activeTab={activeTab}
+        activeTabId={activeTabId}
+        onTabSelected={setActiveTabId}
+      />
     </div>
-  </div>
-);
+  );
+};
