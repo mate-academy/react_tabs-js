e428a3b5a5a4f6597ad0da479f7576fbd09c6bdc
Valentyna <adm@adms-MacBook-Pro.local>
2025-01-31 14:15:04 +0200

    add react tabs js

--- src/components/Tabs/Tabs.spec.jsx
+++ src/components/Tabs/Tabs.spec.jsx
@@ -1,101 +1,42 @@
 /* eslint-disable react/jsx-filename-extension */
-import { mount } from '@cypress/react18';
-import { Tabs } from './Tabs';
+import React from 'react';
 
-const page = {
-  tabs: () => cy.byDataCy('Tab'),
-  content: () => cy.byDataCy('TabContent'),
-  tab: index => page.tabs().eq(index),
-  tabLink: index => page.tab(index).find('a'),
+const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  const handleTabClick = (id, e) => {
+    e.preventDefault();
+    if (id !== activeTab.id) {
+      onTabSelected(id);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={tab.id === activeTab.id ? 'is-active' : ''}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={e => handleTabClick(tab.id, e)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
 };
 
-describe('Tabs component', () => {
-  describe('by default', () => {
-    beforeEach(() => {
-      const tabs = [
-        { id: 'tab-10', title: 'Tab 10', content: 'Some text 10' },
-        { id: 'tab-20', title: 'Tab 20', content: 'Some text 20' },
-        { id: 'tab-30', title: 'Tab 30', content: 'Some text 30' },
-        { id: 'tab-40', title: 'Tab 40', content: 'Some text 40' },
-      ];
-
-      mount(
-        <Tabs
-          tabs={tabs}
-          activeTabId="tab-20"
-          onTabSelected={cy.spy().as('onTabSelected')}
-        />,
-      );
-    });
-
-    it('should render all tabs', () => {
-      page.tabs().should('have.length', 4);
-
-      page.tab(0).should('have.text', 'Tab 10');
-      page.tab(1).should('have.text', 'Tab 20');
-      page.tab(2).should('have.text', 'Tab 30');
-      page.tab(3).should('have.text', 'Tab 40');
-    });
-
-    it('should have a link in each tab', () => {
-      page
-        .tabLink(0)
-        .should('have.text', 'Tab 10')
-        .and('have.attr', 'href', '#tab-10');
-
-      page
-        .tabLink(3)
-        .should('have.text', 'Tab 40')
-        .and('have.attr', 'href', '#tab-40');
-    });
-
-    it('should highlight only a selected tab', () => {
-      page.tab(0).should('not.have.class', 'is-active');
-      page.tab(1).should('have.class', 'is-active');
-      page.tab(2).should('not.have.class', 'is-active');
-      page.tab(3).should('not.have.class', 'is-active');
-    });
-
-    it('should show the content of the selected tab', () => {
-      page.content().should('have.text', 'Some text 20');
-    });
-
-    it('should not invoke onTabSelected callback before click', () => {
-      cy.get('@onTabSelected').should('not.be.called');
-    });
-
-    it('should invoke onTabSelected if another tab is clicked', () => {
-      page.tabLink(3).click();
-
-      cy.get('@onTabSelected').should('be.calledOnce');
-    });
-
-    it('should invoke onTabSelected with id of new selected tab', () => {
-      page.tabLink(3).click();
-
-      cy.get('@onTabSelected').should('be.calledWith', 'tab-40');
-    });
-
-    it('should not invoke onTabSelected if the current tab is clicked', () => {
-      page.tabLink(1).click();
-
-      cy.get('@onTabSelected').should('not.be.called');
-    });
-  });
-
-  describe('', () => {
-    it('should highlight the first tab if activeTabId is wrong', () => {
-      const tabs = [
-        { id: 'tab-10', title: 'Tab 10', content: 'Some text 10' },
-        { id: 'tab-20', title: 'Tab 20', content: 'Some text 20' },
-        { id: 'tab-30', title: 'Tab 30', content: 'Some text 30' },
-        { id: 'tab-40', title: 'Tab 40', content: 'Some text 40' },
-      ];
-
-      mount(<Tabs tabs={tabs} activeTabId="asdasd" onTabSelected={() => {}} />);
-
-      page.tab(0).should('have.class', 'is-active');
-      page.tab(1).should('not.have.class', 'is-active');
-    });
-  });
-});
+export default Tabs;
