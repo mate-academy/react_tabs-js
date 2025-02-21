5f35143bae25bba966b2db9d202cfb4e4e1a10d3
Lucas <soniclord123@gmail.com>
2025-02-21 00:23:11 -0300

    add solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,30 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const validId = tabs.some(tab => activeTabId === tab.id)
+    ? activeTabId
+    : tabs[0].id;
+  /* eslint-disable */
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={validId === tab.id ? 'is-active' : ''}
+              onClick={() => {validId !== tab.id ? onTabSelected(tab.id) : ''}}
+              data-cy="Tab">
+              <a href={`#${tab.id}`} data-cy="TabLink">
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === validId).content}
+      </div>
+    </div>
+  );
+};
