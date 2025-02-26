14f3cf044b0ed0b55a855f218725374fb3bc0812
Ivan Lavruk <ilavr8725@gmail.com>
2025-02-26 19:19:49 +0100

    reat tabs js

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,17 @@
-export const Tabs = () => {};
+export const Tabs = ({ tab, selectedTab, setSelectedTab }) => {
+  return (
+    <li className={tab.id === selectedTab ? 'is-active' : ''} data-cy="Tab">
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={() => {
+          if (tab.id !== selectedTab) {
+            setSelectedTab(tab.id);
+          }
+        }}
+      >
+        {tab.title}
+      </a>
+    </li>
+  );
+};
