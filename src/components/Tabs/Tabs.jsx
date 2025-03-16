a00946afa4b12c99dcd098961b579853fd90b5a4
Kyrylo Bondarenko <dejsk35@gmail.com>
2025-03-16 18:43:07 +0200

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,29 @@
-export const Tabs = () => {};
+import React from 'react';
+import { Tab } from '../Tab/Tab';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const validActiveTabId = tabs.some(tab => tab.id === activeTabId)
+    ? activeTabId
+    : tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <Tab
+              key={tab.id}
+              tab={tab}
+              isActive={tab.id === validActiveTabId}
+              onTabSelected={onTabSelected}
+            />
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === validActiveTabId)?.content}
+      </div>
+    </div>
+  );
+};
