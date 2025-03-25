2578fcc1b2d3d90a1db408d003e197bf7032520f
Andriian <an.hrytsan@gmail.com>
2025-03-25 17:35:36 +0200

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,38 @@
-export const Tabs = () => {};
+import { Fragment } from 'react';
+import 'bulma/css/bulma.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import clsx from 'clsx';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const validActiveTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+            <li
+              key={tab.id}
+              className={clsx({ 'is-active': tab.id === validActiveTab.id })}
+              data-cy="Tab"
+              onClick={() => {
+                if (tab.id !== activeTabId) {
+                  onTabSelected(tab.id);
+                }
+              }}
+            >
+              <a href={`#${tab.id}`} data-cy="TabLink">
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {validActiveTab.content}
+      </div>
+    </>
+  );
+};
