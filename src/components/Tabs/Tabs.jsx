b4a1d047cc3511b7d147e0f5d03d1df7c4505f82
Nataliia Smirnova <ensueno11@gmail.com>
2025-04-06 11:59:27 +0300

    feat: tabs selection

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,41 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const fallbackTabId = tabs[0]?.id;
+  const currentActiveTabId = tabs.some(tab => tab.id === activeTabId)
+    ? activeTabId
+    : fallbackTabId;
+
+  const activeTab = tabs.find(tab => tab.id === currentActiveTabId);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === currentActiveTabId ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={e => {
+                  e.preventDefault();
+                  if (tab.id !== currentActiveTabId) {
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab?.content}
+      </div>
+    </div>
+  );
+};
