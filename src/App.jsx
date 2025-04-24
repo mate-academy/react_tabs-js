6a03e186a91e675b535cb71625657b901a6aee84
PedroWeb10 <pdro_dev@yahoo.com>
2025-04-24 15:53:26 -0300

    React Tabs (JS) 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -7,38 +7,63 @@ export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-4', title: 'Tab 4', content: 'Some text 4' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+const Tabs = ({ tabs: tb, activeTabId, onTabSelected }) => {
+  const activeTab = tb.find(tab => tab.id === activeTabId) || tb[0];
 
+  return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
+          {tb.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === activeTab.id ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={e => {
+                  e.preventDefault();
+                  if (tab.id !== activeTab.id) {
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-        Some text 1
+        {activeTab.content}
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+export const App = () => {
+  const [activeTabId, setActiveTabId] = useState(tabs[0].id);
+
+  const newTabSelected = tabId => {
+    setActiveTabId(tabId);
+  };
+
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div className="section">
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
+      <Tabs
+        tabs={tabs}
+        activeTabId={activeTabId}
+        onTabSelected={newTabSelected}
+      />
+    </div>
+  );
+};
