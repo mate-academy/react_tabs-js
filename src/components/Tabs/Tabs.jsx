6648751c29e36d07a298301611215e95b2c6be9a
Denis Pitsul <denia.pitsul@gmail.com>
2025-05-13 20:32:46 +0300

    add react tabs js solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const validActiveTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  const onTabClick = tabId => {
+    if (tabId !== validActiveTab.id) {
+      onTabSelected(tabId);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }) => (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+            <li
+              className={classNames({ 'is-active': id === validActiveTab.id })}
+              data-cy="Tab"
+              key={id}
+              onClick={() => onTabClick(id)}
+            >
+              <a href={`#${id}`} data-cy="TabLink">
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {validActiveTab.content}
+      </div>
+    </div>
+  );
+};
