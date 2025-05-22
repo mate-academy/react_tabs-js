2709f570fa455384a0486eaea8cf2c78f9c98e36
unknown <vladhafarov@gmail.com>
2025-05-22 14:36:34 +0300

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,53 @@
-export const Tabs = () => {};
+// export const Tabs = () => {};
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const currentActiveTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === currentActiveTab.id ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={event => {
+                  if (tab.id !== currentActiveTab.id) {
+                    event.preventDefault();
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currentActiveTab && currentActiveTab.content}
+      </div>
+    </div>
+  );
+};
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  activeTabId: PropTypes.string.isRequired,
+  onTabSelected: PropTypes.func.isRequired,
+};
