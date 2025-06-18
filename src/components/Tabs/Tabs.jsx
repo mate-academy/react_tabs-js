529706fd735665423510f88b33af9d657bb69298
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2025-06-18 15:53:21 +0300

    the implementation the Tabs component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,41 @@
-export const Tabs = () => {};
+import clsx from 'clsx';
+import { getSelectedTab } from '../../functions';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const selectedTab = getSelectedTab(tabs, activeTabId);
+
+  const { id, content } = selectedTab;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={clsx({ 'is-active': tab.id === id })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                id={tab.id}
+                onClick={event => {
+                  if (event.target.id !== id) {
+                    onTabSelected(event.target.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {content}
+      </div>
+    </div>
+  );
+};
