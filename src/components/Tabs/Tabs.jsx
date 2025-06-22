0b33b6dd64700580842d7859bbaf113eed181d00
Svystun Pavlo <spo26081985@gmail.com>
2025-06-22 15:58:41 +0300

    add solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -7,6 +7,7 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
         <ul>
           {tabs.map(tab => (
             <li
+              key={tab.id}
               className={activeTab.id !== tab.id ? '' : 'is-active'}
               data-cy="Tab"
             >
