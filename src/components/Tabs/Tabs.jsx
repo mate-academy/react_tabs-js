1f35c0388d26b56193020e24ee204ad4dcf48d5d
Taras <luchakt2@gmail.com>
2025-06-23 15:10:03 +0300

    nitial implementation of functional Tabs component as per task requirements

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={tab === activeTab.id ? 'is-active' : ''}
+          data-cy="Tab"
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={(event) => {
+            event.preventDefault();
+
+            if (tab.id !== activeTab.id) {
+              onTabSelected(tab.id);
+            }
+          }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
