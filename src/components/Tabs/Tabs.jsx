ba178b2411f9a12a0fc82c0d81d33a034217e47f
annaabramovaa <abramovaana193@gmail.com>
2025-06-24 12:13:58 +0300

    react-tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,30 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <>
+      <ul>
+        {tabs.map(tab => (
+          <li
+            data-cy="Tab"
+            key={tab.id}
+            className={tab.id === activeTab.id ? 'is-active' : ''}
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => {
+                if (tab.id !== activeTab.id) {
+                  onTabSelected(tab.id);
+                }
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div data-cy="TabContent">{activeTab.content}</div>
+    </>
+  );
+};
