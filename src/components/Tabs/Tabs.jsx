05a898f73f11df8fb4351de7a1633b984aeac568
Sofiia13 <sophiyastanish@gmail.com>
2025-06-27 13:54:17 +0300

    add solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,21 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={activeTabId === tab.id ? 'is-active' : ''}
+          data-cy="Tab"
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={() => onTabSelected(tab.id)}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
