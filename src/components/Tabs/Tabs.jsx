a6e3efc5704a6a4a782c38456fbb2d9ab9b4facf
Eleonora Stasyuk <1990applesweet@gmail.com>
2025-06-30 19:42:26 +0300

    initial

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            className={cn({
+              'is-active': activeTabId === tab.id,
+            })}
+            data-cy="Tab"
+            key={tab.id}
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              id={tab.id}
+              onClick={event => onTabSelected(event.target.id)}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className="block" data-cy="TabContent">
+      Some text {parseInt(activeTabId.match(/\d+/)[0], 10)}
+    </div>
+  </div>
+);
