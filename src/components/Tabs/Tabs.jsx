7ca9f3f7fee1439b44bee5e5db8a9e3ecc63e039
Vanya Holovey <golovejvana9@gmail.com>
2025-07-18 14:52:12 +0300

    react tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                key={tab.id}
+                className={classNames({
+                  'is-active':
+                    tab.id ===
+                    (tabs.find(t => t.id === activeTabId)
+                      ? activeTabId
+                      : tabs[0].id),
+                })}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  onClick={event => {
+                    event.preventDefault();
+                    if (tab.id !== activeTabId) {
+                      onTabSelected(tab.id);
+                    }
+                  }}
+                  data-cy="TabLink"
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {(tabs.find(tab => tab.id === activeTabId) || tabs[0]).content}
+      </div>
+    </div>
+  );
+};
