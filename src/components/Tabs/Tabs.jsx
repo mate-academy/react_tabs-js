d67f6a4e32c3ee21322d44dab206905f06080e58
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-22 19:05:02 +0300

    sol

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,34 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) ?? tabs[0];
+
+  return (
+    <>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={classNames({ 'is-active': tab.id === activeTab.id })}
+          data-cy="Tab"
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={() => {
+              if (tab.id !== activeTabId) {
+                onTabSelected(tab.id);
+              }
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </>
+  );
+}
