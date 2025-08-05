b9d461950f32aabe2063c31e8aff5faa863ad912
Nikolay Tyshchenko <nikolay.tyshchenko04@gmail.com>
2025-08-05 12:00:33 +0300

    solution for react tabs task

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,42 @@
-export const Tabs = () => {};
+import React from 'react';
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const { content } = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+  const currentTabId =
+    tabs.find(tab => tab.id === activeTabId)?.id || tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({
+                'is-active': tab.id === currentTabId,
+              })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (tab.id !== currentTabId) {
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {content}
+      </div>
+    </div>
+  );
+};
