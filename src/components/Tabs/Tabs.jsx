3f0ca22df122ac70a98e0ac99dd0a394b0007410
Oleksandr-Oliferuk <oleksandr.oliferuk@gmail.com>
2025-08-05 13:54:04 +0300

    add solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,27 @@
-export const Tabs = () => {};
+
+import cn from 'classnames';
+
+
+export const Tabs = ({ tab, nameTable, textTable, nameTab }) => {
+  if (!tab) {
+    return null;
+  }
+  const { id, title, content } = tab
+
+  return (
+
+    <li
+      className={cn({ 'is-active': nameTab === title })}
+      data-cy="Tab"
+      >
+      <a onClick={() => {
+        nameTable(title)
+        textTable(content)
+      } }
+        href={`#${id}`}
+        data-cy="TabLink">
+        {title}
+      </a>
+    </li>
+  )
+};
