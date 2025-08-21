bf72feec8a0bc3260103787a0b00cd67bd21ac24
danie010830 <danielgukillwar008@gmail.com>
2025-08-21 19:26:03 -0300

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  if (!tabs || tabs.length === 0) return null;
+
+  // Verifica se activeTabId existe, senão usa o primeiro
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div data-cy="Tabs">
+      <ul>
+        {tabs.map(tab => {
+          const isActive = tab.id === activeTab.id;
+
+          return (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={isActive ? 'is-active' : undefined}
+            >
+              <a
+                data-cy="TabLink"
+                href={`#${tab.id}`}
+                onClick={e => {
+                  e.preventDefault();
+                  if (!isActive && typeof onTabSelected === 'function') {
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      {/* Aqui está o que faltava pros testes */}
+      <h1>{`Selected tab is ${activeTab.title}`}</h1>
+
+      <div data-cy="TabContent">{activeTab.content}</div>
+    </div>
+  );
+};
