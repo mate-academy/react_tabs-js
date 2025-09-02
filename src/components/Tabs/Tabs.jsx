3742305e69d4926dabf62423a8cab4ee8b2bbb7a
gOFFa <n4v4rm0re666@gmail.com>
2025-09-02 19:00:23 +0300

    add partial task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,36 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  function handleTabClick(tabId) {
+    if (activeTabId !== tabId) {
+      onTabSelected(tabId);
+    }
+  }
+
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              key={tab.id}
+              onClick={() => handleTabClick(tab.id)}
+              className={cn({ 'is-active': tab.id === activeTabId })}
+            >
+              <a href={`#${tab.id}`} data-cy="TabLink">
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab ? activeTab.content : null}
+      </div>
+    </div>
+  )
+};
