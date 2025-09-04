6395008ddc4c3025502fed45bdce71c6892b156e
dimon <80713na@gmail.com>
2025-09-04 16:06:21 +0300

    tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,29 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, onTabSelected, activeTabId }) => {
+  return (
+    <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+    <ul>
+      {tabs.map(({ title, id}) => (
+        <li
+          className={`${id === activeTabId ? 'is-active' : ''}`}
+          data-cy="Tab"
+          key={id}
+            onClick={() => { id !== activeTabId ? onTabSelected(id) : '';
+            }}
+          >
+          <a
+            href={`#${id}`}
+            data-cy="TabLink"
+          >
+            {title}
+          </a>
+        </li>
+      ))}
+    </ul>
+    </div>
+    <div className="block" data-cy="TabContent">
+        {tabs.find(el => el.id === activeTabId)?.content}
+      </div>
+    </div>
+  );
+};
