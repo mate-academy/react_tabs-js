31217f95a4e7679f4cca2423da0467e3d265fcb4
MaksymBerber <marcusbmmb@outlook.com>
2025-10-06 21:53:12 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,28 @@
-import React from 'react';
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import './App.scss';
+import { useState, useMemo } from 'react';
+import { Tabs } from './components/Tabs/Tabs';
 
-export const tabs = [
+const tabsData = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+export const App = () => {
+  const [activeTabId, setActiveTabId] = useState(tabsData[0].id);
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
+  const activeTab = useMemo(() => {
+    return tabsData.find(tab => tab.id === activeTabId) || tabsData[0];
+  }, [activeTabId]);
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
+  return (
+    <div className="section p-6 max-w-3xl mx-auto">
+      <h1 className="title mb-6">Selected tab is {activeTab.title}</h1>
 
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="block" data-cy="TabContent">
-        Some text 1
-      </div>
+      <Tabs
+        tabs={tabsData}
+        activeTabId={activeTabId}
+        onTabSelected={setActiveTabId}
+      />
     </div>
-  </div>
-);
+  );
+};
