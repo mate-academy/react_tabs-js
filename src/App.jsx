63b2069687abb87b7605e21f4ad23730a2510140
volodimir-briatko <volodimirbryatko@gmail.com>
2025-10-09 18:58:32 +0300

    solution2

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,17 @@ export const tabs = [
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState('tab-1');
 
-  
-
   const activeTab = tabs.find(tab => tab.id === activeTabId);
-  return (
-  <div className="section">
-    <h1 className="title">Selected tab is {activeTab.title}</h1>
 
-    <Tabs 
-    tabs={tabs} 
-    activeTabId={activeTabId}
-    onTabSelected={(id) => setActiveTabId(id)}
-    />
-  </div>
+  return (
+    <div className="section">
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
+
+      <Tabs
+        tabs={tabs}
+        activeTabId={activeTabId}
+        onTabSelected={id => setActiveTabId(id)}
+      />
+    </div>
   );
 };
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,37 +1,39 @@
-
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
-    const handleClick = (event, id) => {
-        event.preventDefault();
-        onTabSelected(id);
-      }
-    const activeTab = tabs.find(tab => tab.id === activeTabId);  
-    
-    return (
+  const handleClick = (event, id) => {
+    if (id === activeTabId) return;
+    event.preventDefault();
+    onTabSelected(id);
+  };
+
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-        {
-        tabs.map(tab => (
-          <li 
-          key={tab.id}
-          className={activeTabId === tab.id ? 'is-active' : ''} data-cy="Tab"
-          >
-            <a href={`#${tab.id}`} data-cy="TabLink" 
-            onClick={(event) =>{
-              handleClick(event, tab.id); 
-            } }>
-              {tab.title}
-            </a>
-          </li>
-        ))
-      }
-          
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={activeTabId === tab.id ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={event => {
+                  handleClick(event, tab.id);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-      {activeTab.content}
+        {activeTab.content}
       </div>
     </div>
-    );
+  );
 };
