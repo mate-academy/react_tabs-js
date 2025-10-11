3e20502889f1ad809a769c255eae0f19afbf88a8
mahdenko <vladearth7@gmail.com>
2025-10-11 20:08:27 +0300

    Improved by comments

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,11 @@ export const tabs = [
 
 export const App = () => {
   const [activeTab, setActiveTab] = React.useState('tab-1');
+  const active = tabs.find(tab => tab.id === activeTab) || tabs[0];
 
   return (
     <div className="section">
-      <h1 className="title">
-        Selected tab is {tabs.find(tab => tab.id === activeTab)?.title}
-      </h1>
+      <h1 className="title">Selected tab is {active.title}</h1>
       <Tabs tabs={tabs} activeTabId={activeTab} onTabSelected={setActiveTab} />
     </div>
   );
