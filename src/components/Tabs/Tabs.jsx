e1a3ea492f5c9c4a465e76491990d6e2db496469
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2025-10-12 22:12:36 -0300

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,23 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  return tabs.map(tab => (
+    <li
+      key={tab.id}
+      className={tab.id === activeTabId ? 'is-active' : ''}
+      data-cy="Tab"
+    >
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={event => {
+          event.preventDefault();
+
+          if (tab.id !== activeTabId) {
+            onTabSelected(tab.id);
+          }
+        }}
+      >
+        {tab.title}
+      </a>
+    </li>
+  ));
+};
