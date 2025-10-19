b38f97e95e4b9ba7567b2dd2daa1610f5f62ce04
TaniaFedortsi <tania.fdrts@gmail.com>
2025-10-19 21:35:04 +0300

    Creating the tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,41 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={
+                tab.id === activeTabId ||
+                (!tabs.some(t => t.id === activeTabId) && tab.id === tabs[0].id)
+                  ? 'is-active'
+                  : ''
+              }
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={e => {
+                  e.preventDefault();
+                  if (tab.id !== activeTabId) {
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {(tabs.find(tab => tab.id === activeTabId) || tabs[0]).content}
+      </div>
+    </div>
+  );
+};
