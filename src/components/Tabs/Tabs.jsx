0f654505f64f948d93172513588b4ca17a83f6b1
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-10-25 15:26:59 -0300

    DSADAS

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -19,34 +19,3 @@ export const Tabs = ({ tabs, activeTabId, setActiveTab }) => {
     </ul>
   );
 };
-/*
-<li className={activeTabId === 1 ? 'is-active' : ''} data-cy="Tab">
-        <a
-          href="#tab-id"
-          data-cy="TabLink"
-          onClick={() => handleTabSelection(1)}
-        >
-          Tab 1
-        </a>
-      </li>
-
-      <li data-cy="Tab" className={activeTabId === 2 ? 'is-active' : ''}>
-        <a
-          href="#tab-id"
-          data-cy="TabLink"
-          onClick={() => handleTabSelection(2)}
-        >
-          Tab 2
-        </a>
-      </li>
-
-      <li data-cy="Tab" className={activeTabId === 3 ? 'is-active' : ''}>
-        <a
-          href="#tab-id"
-          data-cy="TabLink"
-          onClick={() => handleTabSelection(3)}
-        >
-          Tab 3
-        </a>
-      </li>
-*/
