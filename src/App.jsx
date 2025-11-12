6f9f83a7492b5512f992a47d82f585d0ad9a6395
MiltonMario <miltonneto0010@gmail.com>
2025-11-12 10:35:56 -0300

    fix solution #1

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export const tabs = [
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState('tab-1');
 
-  const activateTabId = tabs.find(tab => tab.id === activeTabId);
+  const activateTabId = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
     <div className="section">
