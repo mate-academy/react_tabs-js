55dad695a256f42ca5e553f9c04cb5c7c6771dff
Ex1stent <142783655+Abdulahhh2005@users.noreply.github.com>
2025-11-20 01:36:26 +0200

    Update Tabs.jsx

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,3 +1,5 @@
+import cn from 'classnames';
+
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
@@ -5,30 +7,25 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => {
-            const isActive = tab.id === activeTab.id;
-
-            return (
-              <li
-                key={tab.id}
-                data-cy="Tab"
-                className={isActive ? 'is-active' : ''}
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={cn({ 'is-active': tab.id === activeTab.id })}
+            >
+              <a
+                data-cy="TabLink"
+                href={`#${tab.id}`}
+                onClick={() => {
+                  if (tab.id !== activeTab.id) {
+                    onTabSelected(tab.id);
+                  }
+                }}
               >
-                <a
-                  data-cy="TabLink"
-                  href={`#${tab.id}`}
-                  onClick={event => {
-                    event.preventDefault();
-                    if (!isActive) {
-                      onTabSelected(tab.id);
-                    }
-                  }}
-                >
-                  {tab.title}
-                </a>
-              </li>
-            );
-          })}
+                {tab.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
 
