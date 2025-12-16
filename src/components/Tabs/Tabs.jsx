40b46764c1718a3d40fbe29e61ee95e9add1309e
friranger <65720979+friranger1@users.noreply.github.com>
2025-12-16 21:08:50 +0200

    Implemented switching between tabs by click and changing related contend in block

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,33 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, setActiveTabId }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {
+            tabs.map((tab) => {
+              return (
+                <li className={activeTabId === tab.id ? 'is-active' : '' }
+                  key={tab.id}
+                  data-cy="Tab"
+                  onClick={() => setActiveTabId(tab.id)}
+                >
+                  <a href={`#${tab.id}`}
+                    data-cy="TabLink"
+                  >
+                    {tab.title}
+                  </a>
+                </li>
+              )
+            })
+
+          }
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+
+    </div>
+  )
+};
