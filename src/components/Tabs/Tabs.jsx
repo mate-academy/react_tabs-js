e51862001b1a2494054354dac5d59d73a2dd54e7
mpodaniev <mpodanev@gmail.com>
2025-12-17 12:20:35 +0200

    create `Tabs` component and manage active tab state dynamically

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,29 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, setActiveTabId, activeTab }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(({ id, title }) => (
+          <li
+            className={cn({ 'is-active': id === activeTabId })}
+            data-cy="Tab"
+            key={id}
+          >
+            <a
+              href={`#${id}`}
+              data-cy="TabLink"
+              onClick={() => setActiveTabId(id)}
+            >
+              {title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {activeTab.content}
+    </div>
+  </div>
+);
