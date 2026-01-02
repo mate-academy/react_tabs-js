b8d7b6d9590e90df61aabaa1dfbd8df40b592980
Tymofii Vasylenko <zrozik@Tymofiis-MacBook-Air.local>
2026-01-02 12:41:00 +0200

    fix: fixed sheir algorytm

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export const App = () => {
 
   return (
     <div className="section">
-      <h1 className="title">{`Selected tab is ${activeTab.title}`}</h1>
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
       <Tabs
         tabs={tabs}
         activeTabId={activeTab.id}
