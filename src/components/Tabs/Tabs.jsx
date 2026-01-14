14525fb5bc8247386151540e13237b977f7b8dbf
Anton <nimachuka@gmail.com>
2026-01-14 17:41:05 +0100

    all tests passed

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,33 @@
-export const Tabs = () => {};
+/* eslint-disable */
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const actualActiveTab = tabs.find(tab => tab.id === activeTabId)
+  ? activeTabId : tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key = {tab.id}
+              data-cy="Tab"
+              className={tab.id === actualActiveTab ? "is-active" : ""}
+            >
+              <a
+                onClick={() => tab.id !== actualActiveTab && onTabSelected(tab.id)}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === actualActiveTab).content}
+      </div>
+    </div>
+  )
+};
