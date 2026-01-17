d9f61cb8f24168a3064808672e66d90d95ec9815
Alex <1inboxna@gmail.com>
2026-01-17 11:55:52 +0200

    commit message

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,36 @@
-export const Tabs = () => {};
+// import { Tab } from '../Tab/Tab';
+
+export const Tabs = ({
+  tabs,
+  activeTabId,
+  onTabSelected,
+  activeTabContent,
+}) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={tab.id === activeTabId ? 'is-active' : ''}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => onTabSelected(tab.id)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTabContent}
+      </div>
+    </div>
+  );
+};
