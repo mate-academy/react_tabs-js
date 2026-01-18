ddc4c7f8cdf29634503f4e207756c59470558b1a
VM93 <vovamuzyka93@gmail.com>
2026-01-18 14:16:22 +0200

    done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+export const Tabs = ({
+  tabs,
+  activeTabId,
+  onTabSelected /* setActiveTabId */,
+}) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId)
+    ? activeTabId
+    : tabs[0]?.id;
+  const activeTabData = tabs.find(tab => tab.id === activeTab);
+
+  const handleTabClick = tabId => {
+    if (tabId !== activeTab) {
+      onTabSelected(tabId);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={activeTab === tab.id ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => handleTabClick(tab.id)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTabData && activeTabData.content}
+      </div>
+    </div>
+  );
+};
