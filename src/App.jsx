183d01c549dc60a95973759cf35278451a3e0ba1
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-02-02 19:30:36 +0100

    refactor: change hardcoded value to first tab id

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ export const tabs = [
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState('');
 
-  if (tabs.every(tab => tab.id !== activeTabId)) return setActiveTabId('tab-1');
+  if (tabs.every(tab => tab.id !== activeTabId))
+    return setActiveTabId(tabs[0].id);
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
