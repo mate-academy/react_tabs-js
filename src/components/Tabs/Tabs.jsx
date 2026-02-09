0ba9cdb5e96dbf4fda89080355431716a88d1ba2
Gianluca Marchese <gianluca.marchese93@gmail.com>
2026-02-09 22:10:45 +0000

    Implement stateless Tabs component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+export const Tabs = ({
+  tabs,
+  activeTabId,
+  onTabSelected,
+}) => {
+  const activeTab =
+    tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div>
+      <ul className="tabs">
+        {tabs.map(tab => {
+          const isActive = tab.id === activeTab.id;
+
+          return (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={isActive ? 'is-active' : ''}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={event => {
+                  event.preventDefault();
+
+                  if (!isActive) {
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
