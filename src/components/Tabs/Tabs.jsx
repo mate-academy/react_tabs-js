560fe47826ea303d6c31e6dd15f798f9bfaae952
Boris Mund <borismund@Mac.lan>
2026-02-21 17:34:41 +0200

    fix find

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,54 @@
-export const Tabs = () => {};
+import { act } from "react"
+
+export const Tabs = ({
+    tabs,
+    activeTabId,
+    onTabSelected
+}) => {
+
+    const activeSearch = tabs.find(tab => tab.id === activeTabId) || tabs[0]
+
+    const selctActive = (click) => {
+        if (click === activeTabId) {
+
+        } else {
+            onTabSelected(click);
+        }
+    }
+
+    return (
+
+        <div data-cy="TabsComponent">
+            <div className="tabs is-boxed">
+                <ul> {
+                    tabs.map(tab =>
+                        <li key={tab.id}
+                            className=
+                            {activeSearch.id === tab.id && "is-active"}
+                            data-cy="Tab">
+                            <a href={`#${tab.id}`}
+                                onClick={(event) => {
+                                    event.preventDefault(); // Canceled action 
+                                    selctActive(tab.id)  // Call my function 
+                                }}
+                                data-cy="TabLink">
+                                {tab.title}
+                            </a>
+                        </li>
+                    )}
+                </ul>
+            </div>
+
+            <div className="block" data-cy="TabContent">
+                {activeSearch.content}
+            </div>
+        </div >
+    )
+}
+
+
+//Если id неверный:
+// 	•	UI должен вести себя так, будто активна первая вкладка.
+
+// Не только по контенту,
+// но и по классу is-active.
