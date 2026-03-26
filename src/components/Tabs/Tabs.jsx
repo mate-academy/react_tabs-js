c5b7f64ae4a456d2a0f203d12e88c18beabae948
Serhii Shimko <shimkosergij2010@gmail.com>
2026-03-26 14:20:58 +0200

    completedthe task

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,51 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const validActiveTabId = tabs.some(tab => activeTabId === tab.id)
+    ? activeTabId
+    : tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                className={cn({
+                  'is-active': tab.id === validActiveTabId,
+                })}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (tab.id !== validActiveTabId) {
+                      onTabSelected(tab.id);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      {tabs.map(tab => {
+        if (tab.id === validActiveTabId) {
+          return (
+            <div key={tab.id} className="block" data-cy="TabContent">
+              {tab.content}
+            </div>
+          );
+        }
+
+        return '';
+      })}
+    </div>
+  );
+};
