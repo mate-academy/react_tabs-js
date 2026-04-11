51bf81218bbec76693ab2d28afe9fc195556ecfd
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-04-11 10:44:55 +0300

    not all test passed, need to check

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,21 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  return (
+    <ul>
+      {tabs.map(tab => {
+        const isActive = tab.id === activeTabId ? `is-active` : '';
+
+        return (
+          <li className={isActive} data-cy="Tab" key={tab.id}>
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => onTabSelected(tab.id)}
+            >
+              {tab.title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
