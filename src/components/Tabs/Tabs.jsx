ece5833f9e3b883d6df9a7acf89b7aeff48ec21c
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-28 19:14:09 +0000

    feat: implement Tabs component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) ?? tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const isActive = tab.id === activeTab.id;
+
+            return (
+              <li
+                data-cy="Tab"
+                key={tab.id}
+                className={isActive ? 'is-active' : ''}
+              >
+                <a
+                  data-cy="TabLink"
+                  href={`#${tab.id}`}
+                  onClick={() => {
+                    if (!isActive) {
+                      onTabSelected(tab.id);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
