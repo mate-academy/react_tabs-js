4e1025291d29782d849d346f976aed2d8f9530b5
Rostyslav <rooostik2016@gmail.com>
2026-05-25 02:57:41 +0300

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,28 +1,36 @@
 import cn from 'classnames';
 
-export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+export const Tabs = ({ tabs = [], activeTabId, onTabSelected }) => {
+  const validTabId = tabs.find(tab => tab.id === activeTabId)?.id || tabs[0].id;
+
   return (
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            className={cn({ 'is-active': tab.id === activeTabId })}
-            data-cy="Tab"
-            key={tab.id}
-          >
-            <a
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-              onClick={event => {
-                event.preventDefault();
-                onTabSelected(tab.id);
-              }}
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({ 'is-active': tab.id === validTabId })}
+              data-cy="Tab"
+              key={tab.id}
             >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={event => {
+                  event.preventDefault();
+                  if (tab.id !== validTabId) onTabSelected(tab.id);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(t => validTabId === t.id).content}
+      </div>
+    </>
   );
 };
