90934a55fd4bb945ca145d18ed26e26f97cbeafb
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-06-02 23:30:37 +0300

    Optimized changes

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,27 @@
-export const Tabs = () => {};
+import cn from "classnames";
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+
+    return (
+        <>
+            {tabs.map(({id, title}) => (
+                <li 
+                    key={id}
+                    className={cn({'is-active' : id === activeTabId})}
+                    data-cy="Tab"
+                >
+                    <a
+                        href={`#${id}`}
+                        data-cy="TabLink"
+                        onClick={() => (
+                            (activeTabId !== id) 
+                            && onTabSelected(id)
+                        )}
+                    >
+                        {title}
+                    </a>
+                </li>
+            ))}
+        </>
+    )
+};
