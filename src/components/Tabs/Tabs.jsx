3d69c753e4cfc0f556588fc5705395729e73ca35
Rodrigo <sistsayap>
2026-07-10 23:45:56 -0300

    Implement Tabs component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+  return (
+    <>
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            data-cy="Tab"
+            className={tab.id === activeTab.id ? 'is-active' : ''}
+          >
+            <a
+              data-cy="TabLink"
+              href={`#${tab.id}`}
+              onClick={() => {
+                if (tab.id !== activeTab.id) {
+                  onTabSelected(tab.id);
+                }
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </>
+  );
+};
