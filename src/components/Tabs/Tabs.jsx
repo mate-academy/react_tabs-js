192dac061cab69c2c8ae56f5945e88177e2aad9b
Darren Rose <thorn086@gmail.com>
2026-07-13 13:21:05 -0500

    feat: Updated stateless component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,42 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const isKnownTab = tabs.some(tab => tab.id === activeTabId);
+  const currentTabId = isKnownTab ? activeTabId : tabs[0].id;
+  const activeTab = tabs.find(tab => tab.id === currentTabId);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                key={tab.id}
+                className={classNames({
+                  'is-active': tab.id === currentTabId,
+                })}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (tab.id !== currentTabId) {
+                      onTabSelected(tab.id);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
