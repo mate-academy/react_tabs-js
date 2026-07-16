c1ad85eb3df31427808f742a8f37a3b8d5cf4679
ivanov-serg <ivanovsereza26@gmail.com>
2026-07-16 14:43:23 +0300

    react_tabs-js

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,11 @@ const tabs = [
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState(tabs[0].id);
 
-  const activeTab =
-    tabs.find(tab => tab.id === activeTabId) || tabs[0];
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
     <div className="section">
-      <h1 className="title">
-        Selected tab is {activeTab.title}
-      </h1>
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
 
       <Tabs
         tabs={tabs}
