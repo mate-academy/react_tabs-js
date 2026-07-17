09afec5a0f545fb176e3fcc966cdbc87cc1b311f
student1232354 <tarasromanenko33@gmail.com>
2026-07-17 14:16:26 +0300

    first update

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,46 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+const Tab = ({ tabId, tabtitle, activeTabId, onTabSelected }) => {
+  return (
+    <li className={cn({ 'is-active': tabId === activeTabId })} data-cy="Tab">
+      <a
+        href={`#${tabId}`}
+        data-cy="TabLink"
+        onClick={event => {
+          event.preventDefault();
+          if (tabId !== activeTabId) {
+            onTabSelected(tabId);
+          }
+        }}
+      >
+        {tabtitle}
+      </a>
+    </li>
+  );
+};
+
+export const Tabs = ({ tabs = [], activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(one => (
+            <Tab
+              key={one.id}
+              tabId={one.id}
+              tabtitle={one.title}
+              activeTabId={activeTab ? activeTab.id : ''}
+              onTabSelected={onTabSelected}
+            />
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
