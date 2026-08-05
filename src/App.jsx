604d891bb46abf4a131b3828c16ce0a0bd8c5a3e
Vladyslav Kukhar <vladislavkukhar66@gmail.com>
2026-08-05 15:56:05 +0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export const tabs = [
 ];
 
 export const App = () => {
-  const [activeTabId, setActiveTabId] = useState(tabs[0]);
+  const [activeTabId, setActiveTabId] = useState(tabs[0].id);
 
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
