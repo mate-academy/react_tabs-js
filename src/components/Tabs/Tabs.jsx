311663b6d395a383210815b0d1d316e459c71613
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-08-19 16:10:27 +0300

    complete task

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,22 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) =>
+  tabs.map(tab => (
+    <li
+      data-cy="Tab"
+      key={tab.id}
+      className={tab.id === activeTabId ? 'is-active' : ''}
+    >
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={() => {
+          if (tab.id !== activeTabId) {
+            return onTabSelected(tab.id);
+          }
+
+          return null;
+        }}
+      >
+        {tab.title}
+      </a>
+    </li>
+  ));
