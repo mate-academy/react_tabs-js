25d5faa45b96ae5ad09c49352d2785241c69cfa6
AchekcSs_ <achekcss@gmail.com>
2026-08-29 13:57:18 +0300

    feat: implement tabs component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const selectedTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }) => (
+            <li
+              key={id}
+              className={selectedTab?.id === id ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${id}`}
+                data-cy="TabLink"
+                onClick={event => {
+                  event.preventDefault();
+
+                  if (id !== selectedTab?.id) {
+                    onTabSelected(id);
+                  }
+                }}
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
