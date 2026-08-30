170fe840c87051b8ca13f4cefb0ee5600aee0201
Illia Sherbak <illiashcherbak6@gmail.com>
2026-08-30 13:41:59 +0300

    ref

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={activeTab.id === tab.id ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                onClick={() => {
+                  if (activeTab.id !== tab.id) {
+                    onTabSelected(tab.id);
+                  }
+                }}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
+  );
+};
