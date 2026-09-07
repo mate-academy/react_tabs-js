b589eebf13b3320610e3f874ebe50f9c60ce7188
annbisha <annskaylove@gmail.com>
2026-09-08 00:57:10 +0300

    m

--- src/components/Tabs/Tabs.spec.jsx
+++ src/components/Tabs/Tabs.spec.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable react/jsx-filename-extension */
 import { mount } from '@cypress/react18';
-import { Tabs } from './Tabs';
+import { Tabs } from '../Tabs';
 
 const page = {
   tabs: () => cy.byDataCy('Tab'),
