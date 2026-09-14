37291e17d9dd88ca7e9c1fceaabd8a86bb51232a
ALex <sasha.hivuk@gmail.com>
2026-09-14 12:25:04 +0300

    react tabs so bad

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({ tabs = [], activeTabId, onTabSelected }) => {
+  const isTabValid = tabs.some(tab => tab.id === activeTabId);
+  const finalActiveId = isTabValid ? activeTabId : tabs[0]?.id || '';
+  const activeTab = tabs.find(tab => tab.id === finalActiveId);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === finalActiveId ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={event => {
+                  event.preventDefault();
+                  if (tab.id !== finalActiveId && onTabSelected) {
+                    onTabSelected(tab.id);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab?.content}
+      </div>
+    </div>
+  );
+};
